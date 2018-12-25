463c7938684b291abc44ab2d25400b3671aaf67a
Charlie Llewellin <charlie@llewellin.net>
2018-12-25 11:01:27 -0500

    etype xml importer custom module

--- web/modules/custom/etype/modules/etype_xml_importer/src/Form/eTypeXMLImporterConfigForm.php
+++ web/modules/custom/etype/modules/etype_xml_importer/src/Form/eTypeXMLImporterConfigForm.php
@@ -8,8 +8,58 @@
 namespace Drupal\etype_xml_importer\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
 
 class eTypeXMLImporterConfigForm extends ConfigFormBase {
+
+  /**
+   * The entity field manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManager
+   */
+  protected $entity_field_manager;
+
+  /**
+   * @var
+   */
+  protected $conf;
+
+  /**
+   * @var
+   */
+  protected $node_type_options = [];
+
+  /**
+   * @var
+   */
+  protected $fields = [];
+
+  /**
+   * eTypeXMLImporterConfigForm constructor.
+   */
+  public function __construct() {
+    parent::__construct($this->configFactory());
+    $this->conf = $this->config('etype_xml_importer.settings');
+    $node_types = NodeType::loadMultiple();
+    foreach ($node_types as $node_type) {
+      $this->node_type_options[$node_type->id()] = $node_type->label();
+    }
+    $this->getFields();
+  }
+
+
+  /**
+   *
+   */
+  protected function getFields() {
+    $this->entity_field_manager = \Drupal::service('entity_field.manager');
+    $fields = $this->entity_field_manager->getFieldDefinitions('node', $this->conf->get('node_type'));
+    $arr = array_keys($fields);
+    foreach ($arr as $key) {
+      $this->fields[] = $key;
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -30,7 +80,6 @@ class eTypeXMLImporterConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('etype_xml_importer.settings');
 
     /* importer settings */
     $form['importer'] = array(
@@ -43,7 +92,23 @@ class eTypeXMLImporterConfigForm extends ConfigFormBase {
       '#title' => $this->t('Import File'),
       '#description' => $this->t('Enter the file name or names to import. Separate multiple files with a comma.'),
       '#size' => 55,
-      '#default_value' => $config->get('import_file'),
+      '#default_value' => $this->conf->get('import_file'),
+    );
+
+    $form['importer']['subhead_field'] = array(
+      '#title' => $this->t('Subhead field'),
+      '#type' => 'select',
+      '#description' => 'The Drupal field to use for the imported subhead.',
+      '#options' => $this->fields,
+      '#default_value' => $this->conf->get('subhead_field'),
+    );
+
+    $form['importer']['byline_field'] = array(
+      '#title' => $this->t('Byline field'),
+      '#type' => 'select',
+      '#description' => 'The Drupal field to use for the imported byline.',
+      '#options' => $this->fields,
+      '#default_value' => $this->conf->get('byline_field'),
     );
 
 
@@ -60,7 +125,15 @@ class eTypeXMLImporterConfigForm extends ConfigFormBase {
       '#title' => $this->t('Base Import Url'),
       '#description' => $this->t('Url from which to import xml.'),
       '#size' => 55,
-      '#default_value' => $config->get('import_url'),
+      '#default_value' => $this->conf->get('import_url'),
+    );
+
+    $form['importer_advanced']['node_type'] = array(
+      '#title' => $this->t('Content Type'),
+      '#type' => 'select',
+      '#description' => $this->t('Choose a content type into which to import stories.'),
+      '#options' => $this->node_type_options,
+      '#default_value' => $this->conf->get('node_type'),
     );
 
     return parent::buildForm($form, $form_state);
@@ -78,10 +151,12 @@ class eTypeXMLImporterConfigForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
     $this->config('etype_xml_importer.settings')
       ->set('import_file', $form_state->getValue('import_file'))
       ->set('import_url', $form_state->getValue('import_url'))
+      ->set('node_type', $form_state->getValue('node_type'))
+      ->set('subhead_field', $form_state->getValue('subhead_field'))
+      ->set('byline_field', $form_state->getValue('byline_field'))
       ->save();
   }
 
