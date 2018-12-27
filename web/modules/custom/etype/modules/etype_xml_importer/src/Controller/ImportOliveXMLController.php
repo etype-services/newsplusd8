b03d77859dc63bcdb769d72fcdaceb486b9ad40d
Charlie Llewellin <charlie@llewellin.net>
2018-12-27 09:00:38 -0500

    updates to importer

--- web/modules/custom/etype/modules/etype_xml_importer/src/Controller/ImportOliveXMLController.php
+++ web/modules/custom/etype/modules/etype_xml_importer/src/Controller/ImportOliveXMLController.php
@@ -8,7 +8,9 @@
 
 namespace Drupal\etype_xml_importer\Controller;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\etype_xml_importer\Plugin\ForceUTF8\Encoding;
+
+require_once (__DIR__ . '/../Plugin/Encoding.php');
+use \ForceUTF8\Encoding;
 
 class ImportFileMissingException extends \Exception {
   /**
@@ -78,12 +80,37 @@ class ImportOliveXMLController {
   /**
    * @var
    */
+  protected $import_classifieds;
+
+  /**
+   * @var
+   */
   protected $lang_code;
 
   /**
    * @var
    */
+  protected $extract_dir;
+
+  /**
+   * @var
+   */
+  protected $values;
+
+  /**
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
   protected $messenger;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entity_type_manager;
+
+  /**
+   * @var
+   */
+  protected $i;
 
   /**
    * ImportOliveXMLController constructor.
@@ -96,12 +123,16 @@ class ImportOliveXMLController {
     $this->lang_code = 'en';
     $this->byline_field = $this->config->get('byline_field');
     $this->subhead_field = $this->config->get('subhead_field');
+    $this->import_classifieds = $this->config->get('import_classifieds');
     $this->messenger = \Drupal::messenger();
+    $this->entity_type_manager = \Drupal::entityTypeManager();
   }
 
   /**
-   * ImportOliveXMLController
-   * @return mixed
+   * @return array
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function ImportOliveXml() {
 
@@ -137,7 +168,7 @@ class ImportOliveXMLController {
 
       $rand = md5(time());
       $zip_file = "/tmp/" . $rand . ".zip";
-      $extract_dir = '/tmp/' . $rand . '/';
+      $this->extract_dir = '/tmp/' . $rand . '/';
 
       /* Copy Zip file from url */
       $import_file = $this->import_url . trim($item);
@@ -151,7 +182,7 @@ class ImportOliveXMLController {
       $zip = new \ZipArchive;
       $res = $zip->open($zip_file);
       if ($res === TRUE) {
-        $zip->extractTo($extract_dir);
+        $zip->extractTo($this->extract_dir);
         $zip->close();
       } else {
         $message = "eType XML Importer could not open Zip Archive " . $zip_file . ".";
@@ -160,7 +191,7 @@ class ImportOliveXMLController {
       }
 
       /* Loop over directory and get the Files */
-      $fileSystemIterator = new \FilesystemIterator($extract_dir);
+      $fileSystemIterator = new \FilesystemIterator($this->extract_dir);
       $entries = array();
       foreach ($fileSystemIterator as $fileInfo) {
         $entry = $fileInfo->getFilename();
@@ -169,11 +200,11 @@ class ImportOliveXMLController {
         }
       }
       /* Loop over found files and do the extraction */
-      $i = 0;
+      $this->i = 0;
       $t = 0;
       if (count($entries) > 0) {
 
-        $values = array(
+        $this->values = array(
           'type' => $this->node_type,
           'uid' => 1,
           'status' => 0,
@@ -186,7 +217,7 @@ class ImportOliveXMLController {
 
           $markup .= "<p>Extracting articles from $entry section.</p>";
 
-          $xml = file_get_contents($extract_dir . $entry);
+          $xml = file_get_contents($this->extract_dir . $entry);
 
           /* throw Exception and return empty page with message if xml is not extractable */
           try {
@@ -206,138 +237,12 @@ class ImportOliveXMLController {
               $item = $stub->item;
               /* xml object processing of stub which contains link, title, and description */
               foreach ($item as $k => $v) {
-                $array = (array) $v;
-
-                // title is not an object if the stub is valid
-                if (!is_object($array['title'])) {
-
-                  // full article is in the linked file
-                  $ar_file =  $array['link'];
-                  $ar_xml = file_get_contents($extract_dir . $ar_file);
-
-                  /* parse article xhtml from link file */
-                  preg_match("/<prism:section>([^<]+)/", $ar_xml, $coincidencias);
-
-                  /* ignore classifieds? */
-                  $etype_xml_import_classifieds = $this->config->get('import_classifieds');
-                  if ($etype_xml_import_classifieds !== 1) {
-                    if ($coincidencias[1] == 'Classifieds') {
-                      continue;
-                    }
-                  }
-                  $array['section'] = $coincidencias[1];
-
-                  preg_match("/<dc:title>([^<]+)/", $ar_xml, $coincidencias);
-                  // echo 'title should be: ' . $coincidencias[1] . "\n";
-                  $array['title'] = substr($coincidencias[1], 0, 255);
-
-                  preg_match("/<prism:coverDate>([^<]+)/", $ar_xml, $coincidencias);
-                  $array['pub_date'] = $coincidencias[1];
-
-                  // s flag makes dot match linebreaks as well
-                  preg_match("'<body>(.*?)</body>'s", $ar_xml, $coincidencias);
-                  $body = $coincidencias[1];
-                  $body = preg_replace("'<xhtml:h1>(.*?)</xhtml:h1>'s","", $body);
-                  $body = preg_replace("'<pam:media>(.*?)</pam:media>'s","", $body);
-                  $body = preg_replace("'<xhtml:p prism:class=\"deck\">(.*?)</xhtml:p>'s", "", $body, 1);
-                  $body = preg_replace("'<xhtml:p prism:class=\"byline\">(.*?)</xhtml:p>'s", "", $body, 1);
-                  $body = preg_replace("/xhtml:([a-z]?)/", "$1", $body);
-                  // fix tags
-                  $array['body'] = trim($body);
-
-                  // get the slugline
-                  preg_match("'<xhtml:p prism:class=\"deck\">(.*?)</xhtml:p>'s", $ar_xml, $coincidencias);
-                  if (isset($coincidencias[1])) {
-                    $array['slugline'] = trim(strip_tags($coincidencias[1]));
-                  } else {
-                    $array['slugline'] = '';
-                  }
-
-                  // get the byline
-                  preg_match("'<xhtml:p prism:class=\"byline\">(.*?)</xhtml:b>'s", $ar_xml, $coincidencias);
-                  if (isset($coincidencias[1])) {
-                    $temp = preg_replace("/<xhtml:br \\/>/", " ", $coincidencias[1]);
-                    $temp = trim(strip_tags($temp));
-                    $temp = preg_replace("/^by\s*/i", "", $temp);
-                    $array['byline'] = ucwords(strtolower($temp));
-                  } else {
-                    $array['byline'] = '';
-                  }
-
-                  // get the pull quote
-                  preg_match("'<xhtml:p prism:class=\"pullQuote\">(.*?)</xhtml:b>'s", $ar_xml, $coincidencias);
-                  if (isset($coincidencias[1])) {
-                    $array['pulled_quote'] = trim(ucwords(strtolower(strip_tags($coincidencias[1]))));
-                  } else {
-                    $array['pulled_quote'] = '';
-                  }
-
-                  /* Images */
-                  $images = array();
-                  preg_match_all("'<pam:media>(.*?)</pam:media>'s", $ar_xml, $coincidencias);
-                  // loop over matches and match data
-                  if (! empty($coincidencias[1])) {
-                    $matches = $coincidencias[1];
-                    foreach ($matches as $item) {
-                      preg_match("/<dc:format>([^<]+)/", $item, $imatches);
-                      if (isset($imatches[1]) && $imatches[1] == 'image/jpg') {
-                        preg_match("'<pam:mediaReference pam:refid=\"(.*)\" />'", $item, $arr);
-                        if (isset($arr[1])) {
-                          $iarray = array();
-                          $iarray['image'] = $arr[1];
-                          preg_match("'<pam:caption>(.*?)</pam:caption>'s", $item, $arr);
-                          if (isset($arr[1])) {
-                            $iarray['caption'] = trim(strip_tags($arr[1]));
-                          } else {
-                            $iarray['caption'] = '';
-                          }
-                          $images[] = $iarray;
-                        }
-                      }
-                    }
-                  }
-
-                  $pub_date = strtotime($array['pub_date']);
-
-                  $node = array(
-                    'title' => Encoding::toUTF8($array['title']),
-                    'summary' => strip_tags(Encoding::toUTF8($array['description'])),
-                    'body' => Encoding::toUTF8($array['body']),
-                    $this->byline_field => substr(Encoding::toUTF8($array['byline']), 0, 255), // trim to field length
-                    $this->subhead_field => Encoding::toUTF8($array['slugline']),
-                    'field_date1' => $pub_date, // Laurel Outlook
-                  );
-
-                  $array = [];
-                  if (count($images) > 0) {
-                    foreach ($images as $image) {
-                      $ipath = $extract_dir . 'img/' . $image['image'];
-                      $array[] = [
-                        'name' => $image['image'],
-                        'path' => $ipath,
-                        'caption' => Encoding::toUTF8($image['caption']),
-                      ];
-                    }
-                    $node['images'] = $array;
-                  }
-
-                  // otherwise field is initiated and shows empty on node page
-                  if (! empty($array['pulled_quote'])) {
-                    $node['field_pulled_quote'] = $array['pulled_quote'];
-                  }
-
-                  //$var = print_r($node, true);
-                  //$markup .= "$var\n\n\n";
-
-                  $values['created'] = $pub_date;
-                  // etype_xml_importer_entity_create($node, $values);
-                  $i++;
-                }
+                $this->ParseItem($v);
               }
             }
           }
-          $markup .= "<p>eType XML Importer found $i articles to import.</p>";
-          $t += $i;
+          $markup .= "<p>eType XML Importer found $this->i articles to import.</p>";
+          $t += $this->i;
         } /* end foreach $entry */
       }
     }
@@ -348,5 +253,163 @@ class ImportOliveXMLController {
 
   }
 
-}
+  /**
+   * @param $item
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function ParseItem($item) {
+    $array = (array) $item;
 
+    // title is not an object if the stub is valid
+    if (!is_object($array['title'])) {
+
+      // full article is in the linked file
+      $ar_file =  $array['link'];
+      $ar_xml = file_get_contents($this->extract_dir . $ar_file);
+
+      /* parse article xhtml from link file */
+      preg_match("/<prism:section>([^<]+)/", $ar_xml, $coincidencias);
+
+      /* ignore if classifieds? */
+      if ($this->import_classifieds !== 1) {
+        if ($coincidencias[1] == 'Classifieds') {
+          return;
+        }
+      }
+      $array['section'] = $coincidencias[1];
+
+      preg_match("/<dc:title>([^<]+)/", $ar_xml, $coincidencias);
+      // echo 'title should be: ' . $coincidencias[1] . "\n";
+      $array['title'] = substr($coincidencias[1], 0, 255);
+
+      preg_match("/<prism:coverDate>([^<]+)/", $ar_xml, $coincidencias);
+      $array['pub_date'] = $coincidencias[1];
+
+      // s flag makes dot match linebreaks as well
+      preg_match("'<body>(.*?)</body>'s", $ar_xml, $coincidencias);
+      $body = $coincidencias[1];
+      $body = preg_replace("'<xhtml:h1>(.*?)</xhtml:h1>'s","", $body);
+      $body = preg_replace("'<pam:media>(.*?)</pam:media>'s","", $body);
+      $body = preg_replace("'<xhtml:p prism:class=\"deck\">(.*?)</xhtml:p>'s", "", $body, 1);
+      $body = preg_replace("'<xhtml:p prism:class=\"byline\">(.*?)</xhtml:p>'s", "", $body, 1);
+      $body = preg_replace("/xhtml:([a-z]?)/", "$1", $body);
+      // fix tags
+      $array['body'] = trim($body);
+
+      // get the slugline
+      preg_match("'<xhtml:p prism:class=\"deck\">(.*?)</xhtml:p>'s", $ar_xml, $coincidencias);
+      if (isset($coincidencias[1])) {
+        $array['slugline'] = trim(strip_tags($coincidencias[1]));
+      } else {
+        $array['slugline'] = '';
+      }
+
+      // get the byline
+      preg_match("'<xhtml:p prism:class=\"byline\">(.*?)</xhtml:b>'s", $ar_xml, $coincidencias);
+      if (isset($coincidencias[1])) {
+        $temp = preg_replace("/<xhtml:br \\/>/", " ", $coincidencias[1]);
+        $temp = trim(strip_tags($temp));
+        $temp = preg_replace("/^by\s*/i", "", $temp);
+        $array['byline'] = ucwords(strtolower($temp));
+      } else {
+        $array['byline'] = '';
+      }
+
+      // get the pull quote
+      preg_match("'<xhtml:p prism:class=\"pullQuote\">(.*?)</xhtml:b>'s", $ar_xml, $coincidencias);
+      if (isset($coincidencias[1])) {
+        $array['pulled_quote'] = trim(ucwords(strtolower(strip_tags($coincidencias[1]))));
+      } else {
+        $array['pulled_quote'] = '';
+      }
+
+      /* Images */
+      $images = array();
+      preg_match_all("'<pam:media>(.*?)</pam:media>'s", $ar_xml, $coincidencias);
+      // loop over matches and match data
+      if (! empty($coincidencias[1])) {
+        $matches = $coincidencias[1];
+        foreach ($matches as $item) {
+          preg_match("/<dc:format>([^<]+)/", $item, $imatches);
+          if (isset($imatches[1]) && $imatches[1] == 'image/jpg') {
+            preg_match("'<pam:mediaReference pam:refid=\"(.*)\" />'", $item, $arr);
+            if (isset($arr[1])) {
+              $iarray = array();
+              $iarray['image'] = $arr[1];
+              preg_match("'<pam:caption>(.*?)</pam:caption>'s", $item, $arr);
+              if (isset($arr[1])) {
+                $iarray['caption'] = trim(strip_tags($arr[1]));
+              } else {
+                $iarray['caption'] = '';
+              }
+              $images[] = $iarray;
+            }
+          }
+        }
+      }
+
+      $pub_date = strtotime($array['pub_date']);
+
+      $node = array(
+        'title' => Encoding::toUTF8($array['title']),
+        'summary' => strip_tags(Encoding::toUTF8($array['description'])),
+        'body' => Encoding::toUTF8($array['body']),
+        $this->byline_field => substr(Encoding::toUTF8($array['byline']), 0, 255), // trim to field length
+        $this->subhead_field => Encoding::toUTF8($array['slugline']),
+        'field_date1' => $pub_date, // Laurel Outlook
+      );
+
+      $array = [];
+      if (count($images) > 0) {
+        foreach ($images as $image) {
+          $ipath = $this->extract_dir . 'img/' . $image['image'];
+          $array[] = [
+            'name' => $image['image'],
+            'path' => $ipath,
+            'caption' => Encoding::toUTF8($image['caption']),
+          ];
+        }
+        $node['images'] = $array;
+      }
+
+      // otherwise field is initiated and shows empty on node page
+      if (! empty($array['pulled_quote'])) {
+        $node['field_pulled_quote'] = $array['pulled_quote'];
+      }
+
+      //$var = print_r($node, true);
+      //$markup .= "$var\n\n\n";
+
+      $this->values['created'] = $pub_date;
+      $this->CreateNode($node);
+      $this->i++;
+    }
+  }
+
+  /**
+   * @param $node
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function CreateNode($node) {
+
+    $storage = $this->entity_type_manager->getStorage('node');
+    $new_entity = $storage->create([
+      'type' => $this->node_type,
+      'title' => $node['title'],
+      'body' => [
+        'value' => $node['body'],
+        'summary' => $node['summary'],
+        'format' => 'full_html',
+      ]
+
+    ]);
+    $new_entity->save();
+  }
+
+}
--- web/modules/custom/etype/modules/etype_xml_importer/src/Plugin/Encoding.php
+++ web/modules/custom/etype/modules/etype_xml_importer/src/Plugin/Encoding.php
@@ -37,7 +37,7 @@ POSSIBILITY OF SUCH DAMAGE.
  * @license  Revised BSD
  */
 
-namespace Drupal\etype_xml_importer\Plugin\ForceUTF8;
+namespace ForceUTF8;
 
 class Encoding {
 
