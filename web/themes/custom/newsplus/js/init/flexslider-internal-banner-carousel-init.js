860aca6bdd82cef2a11d5d726c0e0d215ef803e9
Charlie Llewellin <charlie@llewellin.net>
2019-02-04 16:14:52 -0500

    flexslider

--- web/themes/custom/newsplus/js/init/flexslider-internal-banner-carousel-init.js
+++ web/themes/custom/newsplus/js/init/flexslider-internal-banner-carousel-init.js
@@ -1,12 +1,14 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.mtInternalBannerCarousel = {
     attach: function (context, settings) {
-      $(context).find('#internal-slider-carousel').once('mtInternalBannerCarouselInit').each(function() {
+      $(context).find("#internal-slider-carousel").once("mtInternalBannerCarouselInit").each(function () {
         var $window = $(window), flexslider;
-        // Tiny helper function to add breakpoints
+
+        // Tiny helper function to add breakpoints.
         function getGridSize() {
           return (window.innerWidth < 768) ? 3 : 6;
         }
+
         $(this).flexslider({
           animation: "slide",
           controlNav: false,
@@ -17,14 +19,14 @@
           prevText: "",
           nextText: "",
           asNavFor: "#internal-banner-slider",
-          minItems: getGridSize(), // Use function to pull in initial value
-          maxItems: getGridSize(), // Use function to pull in initial value
-          start: function(slider){
+          minItems: getGridSize(), // Use function to pull in initial value.
+          maxItems: getGridSize(), // Use function to pull in initial value.
+          start: function (slider) {
             flexslider = slider;
           }
         });
-        // check grid size on resize event
-        $window.resize(function() {
+        // Check grid size on resize event.
+        $window.resize(function () {
           var gridSize = getGridSize();
           flexslider.vars.minItems = gridSize;
           flexslider.vars.maxItems = gridSize;
