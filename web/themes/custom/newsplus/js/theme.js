5fa99b8ce42d8aaed5e263ae8ce0f3ca9d02dee9
Charlie Llewellin <charlie@llewellin.net>
2019-02-05 09:00:03 -0500

    fontawesome

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -1,7 +1,16 @@
+/**
+ * @file
+ * Additional theme js.
+ */
+
 (function ($, Drupal) {
-  Drupal.behaviors.newsplusFixes = {
-    attach: function (context, settings) {
-      //
-    }
-  };
+    "use strict";
+    Drupal.behaviors.newsplusFixes = {
+        attach: function (context, settings) {
+            $(".flexslider .slides li img").each(function () {
+                var alt = jQuery(this).attr('alt');
+                console.log(alt);
+            });
+        }
+    };
 })(jQuery, Drupal);
