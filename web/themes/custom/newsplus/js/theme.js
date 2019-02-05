5c9147911fb6f5dcfd97662dd69720bc8122fe60
Charlie Llewellin <charlie@llewellin.net>
2019-02-05 12:09:28 -0500

    fontawesome

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -8,14 +8,14 @@
     Drupal.behaviors.newsplusFixes = {
         attach: function (context, settings) {
             var flexImgList = new Array();
-            $(".flexslider > .slides > li > img").each(function () {
-                var alt = jQuery(this).attr("alt");
-                console.log(alt);
-                if (!flexImgList.includes(alt)) {
-                    jQuery(this).parent().append("<div class=\"flexslider-img-caption\">" + alt + "</div>");
-                    flexImgList.push(alt);
-                }
-                console.log(flexImgList);
+            $("img.image-style-flexslider-full").each(function () {
+                //var alt = jQuery(this).attr("alt");
+                //console.log(alt);
+                //if (!flexImgList.includes(alt)) {
+                //    jQuery(this).parent().append("<div class=\"flexslider-img-caption\">" + alt + "</div>");
+                //    flexImgList.push(alt);
+                //}
+                console.log(jQuery(this));
             });
         }
     };
