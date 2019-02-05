4b54ba8bdee48f7359dcd5fd766260b2622eb406
Charlie Llewellin <charlie@llewellin.net>
2019-02-04 19:58:28 -0500

    flexslider

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -1,12 +1,9 @@
-/* extra theme js */
-
-(function ($) {
-  'use strict';
-  Drupal.behaviors.FontFixer = {
-    attach: function (context, settings) {
-      $("..flex-direction-nav .flex-prev").append('<i class="fas fa-arrow-left" aria-hidden="true"></i>');
-      $("..flex-direction-nav .flex-next").append('<i class="fas fa-arrow-right" aria-hidden="true"></i>');
-    }
-  };
-
-})(jQuery);
+function($, Drupal, drupalSettings) {
+    "use strict";
+    Drupal.behaviors.FontFixer = {
+        attach: function (context, settings) {
+            $(".flex-direction-nav .flex-nav-next").append("<i class=\"fas fa-arrow-left\" aria-hidden=\"true\"></i>");
+            $(".flex-direction-nav .flex-nav-prev").append("<i class=\"fas fa-arrow-right\" aria-hidden=\"true\"></i>");
+        }
+    };
+}(jQuery, Drupal, drupalSettings);
