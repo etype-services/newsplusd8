637823896bfdf2026960fa4640853ccafc994162
Charlie Llewellin <charlie@llewellin.net>
2019-02-05 12:13:26 -0500

    fontawesome

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -8,7 +8,7 @@
     Drupal.behaviors.newsplusFixes = {
         attach: function (context, settings) {
             var flexImgList = new Array();
-            $("img.image-style-flexslider-full").each(function () {
+            $(".node__content > img.image-style-flexslider-full").each(function () {
                 //var alt = jQuery(this).attr("alt");
                 //console.log(alt);
                 //if (!flexImgList.includes(alt)) {
