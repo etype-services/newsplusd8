c89a9baee4b7b73ca4c5bb444f6a720b4e5058d2
Charlie Llewellin <charlie@llewellin.net>
2019-02-04 20:06:49 -0500

    flexslider

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -2,8 +2,8 @@ function($, Drupal, drupalSettings) {
     "use strict";
     Drupal.behaviors.FontFixer = {
         attach: function (context, settings) {
-            $(".flex-direction-nav .flex-nav-next").append("<i class=\"fas fa-arrow-left\" aria-hidden=\"true\"></i>");
-            $(".flex-direction-nav .flex-nav-prev").append("<i class=\"fas fa-arrow-right\" aria-hidden=\"true\"></i>");
+            $(".flex-direction-nav .flex-nav-next a").html("<i class=\"fas fa-arrow-left\" aria-hidden=\"true\"></i>");
+            $(".flex-direction-nav .flex-nav-prev a").html("<i class=\"fas fa-arrow-right\" aria-hidden=\"true\"></i>");
         }
     };
 }(jQuery, Drupal, drupalSettings);
