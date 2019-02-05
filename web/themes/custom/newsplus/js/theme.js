58fcd61e0b8458e27ebbc5904f9640096d27be8e
Charlie Llewellin <charlie@llewellin.net>
2019-02-05 08:25:10 -0500

    fontawesome

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -1,7 +1,6 @@
 (function ($, Drupal) {
   Drupal.behaviors.newsplusFixes = {
     attach: function (context, settings) {
-      alert("es");
       $(".flex-direction-nav .flex-next").append("<i class=\"fas fa-arrow-left\"></i>");
       $(".flex-direction-nav .flex-prev").append("<i class=\"fas fa-arrow-right\"></i>");
     }
