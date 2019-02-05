1250859480f58f07da4502d6af2347be448abb45
Charlie Llewellin <charlie@llewellin.net>
2019-02-05 09:30:21 -0500

    fontawesome

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -7,7 +7,7 @@
     "use strict";
     Drupal.behaviors.newsplusFixes = {
         attach: function (context, settings) {
-            var list = new[]();
+            var list = new Array();
             $(".flexslider > .slides > li > img").each(function () {
                 var alt = jQuery(this).attr("alt");
                 if (list.indexOf(alt) === -1) {
