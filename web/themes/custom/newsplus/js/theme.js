ee127e54c1ac24fad5143ec725f7d1df591c4b97
Charlie Llewellin <charlie@llewellin.net>
2019-02-06 09:14:17 -0500

    layout fixes

--- web/themes/custom/newsplus/js/theme.js
+++ web/themes/custom/newsplus/js/theme.js
@@ -7,7 +7,7 @@
     "use strict";
     Drupal.behaviors.newsplusFixes = {
         attach: function (context, settings) {
-            //
+            $(".block-dfp").find("*").css("max-width", "100%");
         }
     };
 })(jQuery, Drupal);
