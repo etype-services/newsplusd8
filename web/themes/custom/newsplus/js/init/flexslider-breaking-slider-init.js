35c11bc0314d242148483675eeaa0a66807cb6d7
Charlie Llewellin <charlie@llewellin.net>
2019-03-01 18:57:29 +0000

    breaking news

--- web/themes/custom/newsplus/js/init/flexslider-breaking-slider-init.js
+++ web/themes/custom/newsplus/js/init/flexslider-breaking-slider-init.js
@@ -1,20 +1,22 @@
-jQuery(document).ready(function ($) {
-    "use strict";
-    var obj = S("#block-views-block-breaking-news-block-1 .item-list .flexslider");
-    if (obj.length > 0) {
-        $(window).load(function () {
-            obj.fadeIn("slow");
-            obj.flexslider({
-                animation: drupalSettings.newsplus.flexsliderBreakingSliderInit.breakingEffect,
-                slideshowSpeed: drupalSettings.newsplus.flexsliderBreakingSliderInit.breakingEffectTime,
-                prevText: "",
-                nextText: "",
-                pauseOnAction: false,
-                useCSS: false,
-                controlNav: false,
-                directionNav: false
-            });
+/**
+ * @file
+ * Add flexslider functionality to breaking news block.
+ */
 
+jQuery(document).ready(function () {
+    "use strict";
+    var obj = jQuery("#block-views-block-breaking-news-block-1 .item-list .flexslider");
+    if (obj.length > 0) {
+        obj.fadeIn("slow");
+        obj.flexslider({
+            animation: drupalSettings.newsplus.flexsliderBreakingSliderInit.breakingEffect,
+            slideshowSpeed: drupalSettings.newsplus.flexsliderBreakingSliderInit.breakingEffectTime,
+            prevText: "",
+            nextText: "",
+            pauseOnAction: false,
+            useCSS: false,
+            controlNav: false,
+            directionNav: false
         });
     }
 });
