9e6efbb3a84ea1bf02550a6ad7212076c5e577a6
Charlie Llewellin <charlie@llewellin.net>
2019-03-21 13:54:49 +0000

    classified importer

--- web/modules/custom/etype/modules/etype_classified_importer/src/Controller/ImportClassifiedController.php
+++ web/modules/custom/etype/modules/etype_classified_importer/src/Controller/ImportClassifiedController.php
@@ -161,6 +161,7 @@ class ImportClassifiedController {
         'id' => $item->ItemId,
         'category' => $item->categoryId,
       ];
+      $this->createNode($data);
     }
 
     return ['#markup' => ''];
