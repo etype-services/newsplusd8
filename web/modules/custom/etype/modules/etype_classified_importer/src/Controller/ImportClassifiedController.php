32a1484ed9683e88f034dcf682bbbd8248b763b4
Charlie Llewellin <charlie@llewellin.net>
2019-04-01 12:43:36 -0500

    ad css and etype classified importer

--- web/modules/custom/etype/modules/etype_classified_importer/src/Controller/ImportClassifiedController.php
+++ web/modules/custom/etype/modules/etype_classified_importer/src/Controller/ImportClassifiedController.php
@@ -182,8 +182,7 @@ class ImportClassifiedController {
         'created'  => $item->StartDate,
       ]);
       $node->save();
-      print $ad_cat;
-      var_dump($node->field_ad_category);
+      dpm($node);
 
       if ($ad_cat > 0) {
         $node->field_ad_category->target_id = $ad_cat;
