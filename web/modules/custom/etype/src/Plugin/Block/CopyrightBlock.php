4c46dfee00eb213c78137a8fae6054da609cc711
Charlie Llewellin <charlie@llewellin.net>
2019-04-20 14:34:36 -0500

    tiempos theme

---
+++ web/modules/custom/etype/src/Plugin/Block/CopyrightBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\etype\Plugin\Block;
+
+use Drupal;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a Copyright Block.
+ *
+ * @Block(
+ *   id = "copyright_block",
+ *   admin_label = @Translation("Copyright Notice"),
+ *   category = @Translation("eType"),
+ * )
+ */
+class CopyrightBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Uses Markup rather than '#markup' to return raw html.
+   */
+  public function build() {
+    $config = Drupal::config('system.site');
+    $name = $config->get('name');
+    $output = '<p>&copy;&nbsp;' . date("Y") . '&nbsp;' . $name . '</p>';
+    return [
+      '#children' => Markup::create($output),
+    ];
+  }
+
+}
