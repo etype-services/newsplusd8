6deb4627dd563b0ae1a1b89a8f33eb9646b68186
Charlie Llewellin <charlie@llewellin.net>
2019-04-22 22:01:17 -0500

    tiemposcni theme

--- web/themes/custom/tiemposcni/js/extend.js
+++ web/themes/custom/tiemposcni/js/extend.js
@@ -2,8 +2,10 @@
     "use strict";
     Drupal.behaviors.tiemposcniBehavior = {
         attach: function (context, settings) {
-            var w = $(".navbar-end").width;
+            var w = $(".navbar-end").width();
+            console.log(w);
             var pad = (w / 2);
+            console.log(pad);
             $("#main-navbar-menu").css("padding-left", pad + "px");
         }
     };
