113327152b6ba5708962eb83a4f2c7596630ae1a
Charlie Llewellin <charlie@llewellin.net>
2019-04-22 21:48:12 -0500

    tiemposcni theme

---
+++ web/themes/custom/tiemposcni/js/extend.js
@@ -0,0 +1,38 @@
+(function ($, Drupal) {
+    "use strict";
+    Drupal.behaviors.tiemposBehavior = {
+        attach: function (context, settings) {
+            var caption = $(".page-node-type-article .is-three-quarters img").attr("alt");
+
+            /* Menu toggle. */
+            $(".navbar-burger").once("tiemposBehavior").click(function () {
+                $(".navbar-start").toggleClass("is-really-invisible");
+            });
+
+            /* Search form. (No ids because the form is repeated.) */
+            $(".search-edit-submit").once("tiemposBehavior").click(function () {
+                var keys = $('[data-drupal-selector="search-edit-keys"]');
+                var reg = /[a-z]+/;
+                if (keys.hasClass("is-really-invisible")) {
+                    keys.removeClass("is-really-invisible").focus();
+                } else {
+                    if (reg.test(keys.val())) {
+                        $('[data-drupal-selector="search-block-form"]').submit();
+                    } else {
+                        keys.addClass("is-really-invisible");
+                    }
+                }
+            });
+
+            /* Add footer menu to user-menu */
+            $(".footer-menu li > a").once("tiemposBehavior").clone().addClass("is-hidden-desktop").appendTo($(".user-menu"));
+
+            /* Add arrows to section header. */
+            $(".section-tag-heading").once("tiemposBehavior").append("&nbsp;<i class=\"fas fa-angle-right\"></i>");
+
+            /* Image captions */
+            $(".main-caption").html(caption);
+
+        }
+    };
+})(jQuery, Drupal);
