b8b1ada8bd7693289e5ad19fa44814d40c79f6d9
Charlie Llewellin <charlie@llewellin.net>
2019-04-26 14:23:04 -0500

    tiempos theme

---
+++ web/themes/custom/tiempos/theme-settings.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ *
+ * Tiempos theme settings.
+ */
+
+/**
+ * Add themes settings.
+ *
+ * @param array $form
+ *   Form array.
+ * @param array $form_state
+ *   Form state array.
+ */
+function tiempos_form_system_theme_settings_alter(array &$form, array &$form_state) {
+
+  $form['theme_settings']['inverted_logo'] = [
+    '#type'     => 'managed_file',
+    '#title'    => t('Inverted logo for Feature pages with dark background.'),
+    '#required' => FALSE,
+    '#upload_location' => file_default_scheme(),
+    '#default_value' => theme_get_setting('inverted_logo'),
+    '#upload_validators' => [
+      'file_validate_extensions' => ['gif png jpg jpeg'],
+    ],
+  ];
+
+}
