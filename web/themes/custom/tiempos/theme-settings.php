c9085667a1d8cca6c38815a6ad0ec2d9115de8da
Charlie Llewellin <charlie@llewellin.net>
2019-04-26 14:56:50 -0500

    tiempos theme

--- web/themes/custom/tiempos/theme-settings.php
+++ web/themes/custom/tiempos/theme-settings.php
@@ -6,6 +6,8 @@
  * Tiempos theme settings.
  */
 
+use Drupal\file\Entity\File;
+
 /**
  * Add themes settings.
  *
@@ -34,4 +36,33 @@ function tiempos_form_system_theme_settings_alter(array &$form, &$form_state) {
     ],
   ];
 
+  $form['#submit'][] = 'tiempos_settings_form_submit';
+  $themes = list_themes();
+  $active_theme = $GLOBALS['theme_key'];
+  $form_state['build_info']['files'][] = str_replace("/$active_theme.info", '', $themes[$active_theme]->filename) . '/theme-settings.php';
+
+}
+
+/**
+ * Add themes settings.
+ *
+ * @param array $form
+ *   Form array.
+ * @param object $form_state
+ *   Form state array.
+ */
+function tiempos_settings_form_submit(array &$form, $form_state) {
+  $image_fid = $form_state['values']['inverted_logo'];
+  $image = File::load($image_fid);
+  if (is_object($image)) {
+    // Check to make sure that the file is set to be permanent.
+    if ($image->status == 0) {
+      // Update the status.
+      $image->status = FILE_STATUS_PERMANENT;
+      // Save the update.
+      file_save($image);
+      // Add a reference to prevent warnings.
+      file_usage_add($image, 'MYTHEME', 'theme', 1);
+    }
+  }
 }
