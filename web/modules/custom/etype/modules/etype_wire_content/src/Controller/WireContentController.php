e4b72afb93424a1e541934e41a315ba8cc09b9e8
Charlie Llewellin <charlie@llewellin.net>
2019-04-27 09:54:04 -0500

    wire module

--- web/modules/custom/etype/modules/etype_wire_content/src/Controller/WireContentController.php
+++ web/modules/custom/etype/modules/etype_wire_content/src/Controller/WireContentController.php
@@ -3,6 +3,8 @@
 namespace Drupal\etype_wire_content\Controller;
 
 use Drupal;
+use Drupal\Core\Database\Database;
+use Drupal\etype_wire_content\Form\WireConnectionException;
 
 /**
  * Class WireContentController.
@@ -40,6 +42,29 @@ class WireContentController {
    *   Nodes.
    */
   public function wireContent() {
+    $check = _etype_wire_content_check_connection();
+    /* throw Exception and return empty page with message if the wire database setings are missing */
+    try {
+      if ($check === 0) {
+        throw new WireConnectionException();
+      }
+    }
+    catch (WireConnectionException $e) {
+      $this->messenger->addMessage($e->getMessage(), $this->messenger::TYPE_ERROR);
+      return ['#markup' => ''];
+    }
+
+    /* Connect to wire database and get settings. */
+    Database::setActiveConnection('wire');
+    $db = Database::getConnection();
+    $result = $db->select('node', 'n')->fields('n', ['title'])
+      ->execute()
+      ->fetchAll();
+    kint($result);
+
+
+    /* Reset connection. */
+    Database::setActiveConnection();
     return ['#markup' => '<p>Hello.</p>'];
   }
 
