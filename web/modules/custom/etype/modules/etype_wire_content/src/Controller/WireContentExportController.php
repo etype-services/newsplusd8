c72621323ef955f4976d3a8571d462b92d89c6d2
Charlie Llewellin <charlie@llewellin.net>
2019-04-28 09:28:51 -0500

    wire content

--- web/modules/custom/etype/modules/etype_wire_content/src/Controller/WireContentExportController.php
+++ web/modules/custom/etype/modules/etype_wire_content/src/Controller/WireContentExportController.php
@@ -3,6 +3,7 @@
 namespace Drupal\etype_wire_content\Controller;
 
 use Drupal;
+use Drupal\Core\Database\Database;
 
 /**
  * Class WireContentExportController.
@@ -37,9 +38,37 @@ class WireContentExportController {
    * Export Wire Content.
    *
    * @return array
-   *   Nodes.
+   *   Node Export Status in markup.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   public function exportWireContent() {
+    /* Find nodes to export. */
+    $nids = Drupal::entityQuery('node')
+      ->condition('type', 'article')
+      ->condition('field_section', [1, 2, 10], "IN")
+      ->condition('status', '1')
+      ->sort('changed', 'DESC')
+      ->range(0, 20)
+      ->execute();
+    $storage_handler = Drupal::entityTypeManager()->getStorage('node');
+    $nodes = $storage_handler->loadMultiple($nids);
+    if (count($nodes) > 0) {
+      /* Delete current matching rows in wire.node */
+      $config = Drupal::config('system.site');
+      $site_name = $config->get('name');
+      Database::setActiveConnection('wire');
+      $db = Database::getConnection();
+      $db->delete('node')
+        ->condition('site_name', $site_name)
+        ->execute();
+      foreach ($nodes as $node) {
+        var_dump($node->get('title')->value);
+      }
+      /* Reset connection. */
+      Database::setActiveConnection();
+    }
     return ['#markup' => '<p>Hello.</p>'];
   }
 
