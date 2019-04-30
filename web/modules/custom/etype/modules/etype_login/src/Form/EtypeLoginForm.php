ba903d6178a90b54de3f87f13003a3679d158979
Charlie Llewellin <charlie@llewellin.net>
2019-04-30 09:36:42 -0500

    etype login

--- web/modules/custom/etype/modules/etype_login/src/Form/EtypeLoginForm.php
+++ web/modules/custom/etype/modules/etype_login/src/Form/EtypeLoginForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\etype_login\Form;
 
+use Drupal;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use SoapClient;
@@ -29,13 +30,19 @@ class EtypeLoginForm extends FormBase {
       '#type' => 'textfield',
       '#title' => $this->t('User Name'),
       '#required' => TRUE,
+      '#attributes' => ['tabindex' => 20],
+      '#default_value' => 'Jamculp',
     ];
 
     $form['password'] = [
-      '#type' => 'password',
+      '#type' => 'textfield',
       '#title' => $this->t('Password'),
       '#required' => TRUE,
+      '#attributes' => ['tabindex' => 21],
+      '#default_value' => 'Bruno1941',
     ];
+
+    $form['#attached']['library'][] = 'etype_login/etype_login';
 
     $form['actions']['#type'] = 'actions';
 
@@ -52,6 +59,28 @@ class EtypeLoginForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $client = new soapclient('https://www.etypeservices.com/service_GetPublicationIDByUserName.asmx?WSDL');
+    $param = ['UserName' => $form_state->getValue('username')];
+    $response = $client->GetPublicationID($param);
+    $code = $response->GetPublicationIDResult;
+    switch ($code) {
+      case "-9":
+        $message = "We‘re sorry, either your user name or password is incorrect.";
+        Drupal::messenger()->addMessage($message);
+        $form_state->setRebuild();
+        break;
+
+      case "5239":
+        $param['Password'] = $form_state->getValue('password');
+        $client = new soapclient('https://www.etypeservices.com/Service_SubscriberLogin.asmx?WSDL');
+        $response = $client->ValidateSubscriber($param);
+        $param1 = ['UserName' => $form_state->getValue('username')];
+        $client1 = new soapclient('https://www.etypeservices.com/Get_EmailbyUserName.asmx?WSDL');
+        $response1 = $client1->GetSubscriberEmail($param1);
+        //kint($response);
+        //kint($response1);
+        break;
+    }
+
   }
 
 }
