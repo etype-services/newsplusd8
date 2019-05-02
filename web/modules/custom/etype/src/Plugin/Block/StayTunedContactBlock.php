d3c8753ed55f9f9919078e69f1962f2c297f4175
Charlie Llewellin <charlie@llewellin.net>
2019-05-01 20:43:35 -0500

    add block

---
+++ web/modules/custom/etype/src/Plugin/Block/StayTunedContactBlock.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\etype\Plugin\Block;
+
+use Drupal;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a Stay Tuned Header Block.
+ *
+ * @Block(
+ *   id = "staytunedhcontact_block",
+ *   admin_label = @Translation("Social Icon Block for Contact Page in News+ theme"),
+ *   category = @Translation("eType"),
+ * )
+ */
+class StayTunedContactBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Uses Markup rather than '#markup' to return raw html.
+   */
+  public function build() {
+
+    $config = Drupal::config('etype.adminsettings');
+    $links = [];
+    $links['facebook'] = $config->get('facebook');
+    $links['twitter'] = $config->get('twitter');
+    $output = '<h3 class="title">Stay tuned with us</h3>
+<p>You can connect with us through our social media accounts by clicking on a logo.</p>
+<<ul class="icons-list large">';
+    foreach ($links as $k => $v) {
+      if (!empty($v)) {
+        $output .= "<li><a href=\"$v\"><i class=\"fa fa-$k\"></i></a><span class=\"sr-only\">" . ucfirst($k) . "</span></a></li>";
+      }
+    }
+    $output .= '</ul>';
+    return [
+      '#children' => Markup::create($output),
+    ];
+  }
+
+}
