7e597a67a331d6fcf94cceae998f1653b446dc85
Charlie Llewellin <charlie@llewellin.net>
2019-05-05 20:12:57 -0600

    olive xml importer

--- web/modules/custom/etype/modules/etype_xml_importer/src/Controller/ImportOliveXMLController.php
+++ web/modules/custom/etype/modules/etype_xml_importer/src/Controller/ImportOliveXMLController.php
@@ -448,6 +448,8 @@ class ImportOliveXMLController {
         'summary' => $node['summary'],
         'format' => 'full_html',
       ],
+      $this->bylineField => $node[$this->bylineField],
+      $this->subheadField => $node[$this->subheadField],
       $this->imageField => $field_image,
       'uid' => $this->config->get('uid'),
       'status' => 0,
