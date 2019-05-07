b29cf41c8abfec9e9fbcb130264050075f47fec1
Charlie Llewellin <charlie@llewellin.net>
2019-05-06 18:48:55 -0600

    default author

--- web/modules/custom/etype/js/etype.js
+++ web/modules/custom/etype/js/etype.js
@@ -3,7 +3,9 @@
     Drupal.behaviors.etypeloginBehavior = {
         attach: function (context, settings) {
             var author = drupalSettings.etype.author;
-            $("#edit-uid-0-target-id").val(author);
+            if (author.length > 0) {
+                $("#edit-uid-0-target-id").val(author);
+            }
         }
     };
 })(jQuery, Drupal);
