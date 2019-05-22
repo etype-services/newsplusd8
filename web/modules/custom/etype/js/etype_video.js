1650be293e3bc9198b70f7ee56fd63cb2e802cf0
Charlie Llewellin <charlie@llewellin.net>
2019-05-22 07:24:16 -0600

    Video

--- web/modules/custom/etype/js/etype_video.js
+++ web/modules/custom/etype/js/etype_video.js
@@ -2,6 +2,7 @@
     "use strict";
     Drupal.behaviors.etypeAddVideo = {
         attach: function (context, settings) {
+            /* Autofill YouTube ID field */
             $("#edit-field-video-embed-0-value").blur(function (){
                 var res = $("#edit-field-video-embed-0-value").val().split("=");
                 $("#edit-field-mt-bg-video-youtube-0-value").val(res[1]);
