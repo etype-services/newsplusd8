34eb2edfa96b1a641028c8cb84691e1074ce6b4c
Charlie Llewellin <charlie@llewellin.net>
2019-05-25 16:19:26 -0400

    images in front page sections

---
+++ web/themes/custom/tiempos/js/front_page_sections.js
@@ -0,0 +1,24 @@
+(function ($, Drupal) {
+    "use strict";
+    Drupal.behaviors.tiemposHomePage = {
+        attach: function (context, settings) {
+            $(".front-page-sections").each(function () {
+                var obj = this;
+                var height;
+                var heights = [];
+                var maxHeight;
+                $("img", obj).each(function () {
+                    height = $(this).height();
+                    // console.log(height);
+                    heights.push(parseInt(height));
+                });
+                // console.log(heights);
+                maxHeight = Math.max.apply(Math, heights);
+                // console.log(maxHeight);
+                if (maxHeight > 0) {
+                    $(".image-container", obj).css("height", maxHeight);
+                }
+            });
+        }
+    };
+})(jQuery, Drupal);
