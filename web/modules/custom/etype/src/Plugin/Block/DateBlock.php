4baf9371436735a41cfc2fc22ec6746faff3a898
Charlie Llewellin <charlie@llewellin.net>
2019-05-31 08:54:12 -0500

    adding date block

---
+++ web/modules/custom/etype/src/Plugin/Block/DateBlock.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\etype\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a Date Block.
+ *
+ * @Block(
+ *   id = "date_block",
+ *   admin_label = @Translation("Date"),
+ *   category = @Translation("eType"),
+ * )
+ */
+class DateBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Uses Markup rather than '#markup' to return raw html.
+   */
+  public function build() {
+
+    $output = '
+<p class="is-size-7">{{ "now"|date("F j, Y") }}</p>
+<div><a role="button" class="navbar-burger is-hidden-desktop" aria-label="menu" aria-expanded="false"><i class="fas fa-bars"></i></a></div>
+    ';
+
+    return [
+      '#children' => Markup::create($output),
+    ];
+  }
+
+}
