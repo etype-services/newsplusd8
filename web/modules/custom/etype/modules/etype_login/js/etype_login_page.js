481daf671e1120b7b7b5a457939fa31ce0df7ef1
Charlie Llewellin <charlie@llewellin.net>
2019-06-08 22:51:15 -0500

    add extra home page block

--- web/modules/custom/etype/modules/etype_login/js/etype_login_page.js
+++ web/modules/custom/etype/modules/etype_login/js/etype_login_page.js
@@ -2,6 +2,7 @@
     "use strict";
     Drupal.behaviors.etypeloginPageBehavior = {
         attach: function (context, settings) {
+            /* Wildcard to allow for domain in url */
             $('a[href*="/etype-login"]').click(function () {
                 Cookies.set("redirectDestination", window.location.pathname);
             });
