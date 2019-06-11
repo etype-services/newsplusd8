b9cf1548084ee90bf7c7ab3778cfc28397f35cd1
Charlie Llewellin <charlie@llewellin.net>
2019-06-11 10:04:30 -0500

    my account page

--- web/themes/custom/tiempos/js/front_page_sections.js
+++ web/themes/custom/tiempos/js/front_page_sections.js
@@ -2,22 +2,28 @@
     "use strict";
     Drupal.behaviors.tiemposHomePage = {
         attach: function (context, settings) {
-            $(".front-page-sections").each(function () {
-                var obj = this;
-                var height;
-                var heights = [];
-                var maxHeight;
-                $("img", obj).each(function () {
-                    height = $(this).height();
-                    // console.log(height);
-                    heights.push(parseInt(height));
+            var imageFixer = function () {
+                $(".front-page-sections").each(function () {
+                    var obj = this;
+                    var height;
+                    var heights = [];
+                    var maxHeight;
+                    $("img", obj).each(function () {
+                        height = $(this).height();
+                        // console.log(height);
+                        heights.push(parseInt(height));
+                    });
+                    // console.log(heights);
+                    maxHeight = Math.min.apply(Math, heights);
+                    // console.log(maxHeight);
+                    if (maxHeight > 0) {
+                        $(".image-container img", obj).css("height", maxHeight);
+                    }
                 });
-                // console.log(heights);
-                maxHeight = Math.min.apply(Math, heights);
-                // console.log(maxHeight);
-                if (maxHeight > 0) {
-                    $(".image-container img", obj).css("height", maxHeight);
-                }
+            };
+            imageFixer();
+            $(window).resize(function() {
+                imageFixer();
             });
         }
     };
