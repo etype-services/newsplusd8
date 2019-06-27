e704ee443f2b640944577b30223f5169e7098eaa
Charlie Llewellin <charlie@llewellin.net>
2019-06-27 11:29:20 -0500

    add CNI login module

--- web/modules/custom/etype/modules/etype_login_cni/js/etype_login_cni.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal) {
-    "use strict";
-    Drupal.behaviors.etypeloginBehavior = {
-        attach: function (context, settings) {
-            $("#edit-username").attr("tabindex", "20").focus();
-            $("#edit-password").attr("tabindex", "21");
-        }
-    };
-})(jQuery, Drupal);
