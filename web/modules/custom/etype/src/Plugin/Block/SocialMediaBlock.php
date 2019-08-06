2703b8699b5d9ef7bf1daabf3a7ce9980c345f93
Charlie Llewellin <charlie@llewellin.net>
2019-08-06 16:59:43 +0100

    ada compliance

--- web/modules/custom/etype/src/Plugin/Block/SocialMediaBlock.php
+++ web/modules/custom/etype/src/Plugin/Block/SocialMediaBlock.php
@@ -32,7 +32,7 @@ class SocialMediaBlock extends BlockBase {
     $output = '';
     foreach ($links as $k => $v) {
       if (!empty($v)) {
-        $output .= "<a href=\"$v\" class=\"has-text-grey-dark\" target=\"_blank\"><i class=\"fab fa-$k is-size-3\"></i></a>";
+        $output .= "<a href=\"$v\" class=\"has-text-grey-dark\" target=\"_blank\"><i class=\"fab fa-$k is-size-3\"></i><span class=\"sr-only\">$k</span></a>";
       }
     }
     return [
