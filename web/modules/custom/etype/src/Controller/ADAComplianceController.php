e7bf060eb56dbfe94b4a9882515147cf0e6088bd
Charlie Llewellin <charlie@llewellin.net>
2019-08-25 14:38:32 -0500

    ada compliance tester

--- web/modules/custom/etype/src/Controller/ADAComplianceController.php
+++ web/modules/custom/etype/src/Controller/ADAComplianceController.php
@@ -3,6 +3,7 @@
 namespace Drupal\etype\Controller;
 
 use PDOException;
+use PDO;
 
 /**
  * Class ADAComplianceController.
@@ -17,16 +18,34 @@ class ADAComplianceController {
    * @package Drupal\etype\Controller
    */
   public function adaCompliance() {
+
+    $output = '';
+    $fila = [];
+
     try {
-      $mbd = new PDO('mysql:host=localhost;', 'root', '');
-      foreach ($mbd->query('SELECT * from FOO') as $fila) {
-        print_r($fila);
+      $mbd = new PDO('mysql:host=localhost', 'drupal', 'ENX4gVfx<w+1I5~XPb-p.1{H(c3F_]');
+      foreach ($mbd->query('show databases') as $fila) {
+        $output .= var_export($fila["Database"], TRUE);
       }
-      $mbd = NULL;
-    } catch PDOException $e) {
+    }
+    catch (PDOException $e) {
       print "¡Error!: " . $e->getMessage() . "<br/>";
       die();
     }
+
+    $fixed = array_unique($fila);
+
+    foreach ($fixed as $item) {
+      foreach ($mbd->query("SELECT `body_value` FROM $item.block_content__body WHERE `body_value` REGEXP '<iframe[^>]*>'") as $row) {
+        $output .= var_export($row["body_value"], TRUE);
+      }
+    }
+
+    $build = [
+      '#markup' => $output,
+    ];
+
+    return $build;
   }
 
 }
