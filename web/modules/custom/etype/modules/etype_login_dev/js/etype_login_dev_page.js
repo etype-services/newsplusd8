a675ba4b2bb87a06370fd117a5ce208d5a7c8049
Charlie Llewellin <charlie@llewellin.net>
2019-08-26 14:25:03 -0500

    ada compliance

--- web/modules/custom/etype/modules/etype_login_dev/js/etype_login_dev_page.js
+++ web/modules/custom/etype/modules/etype_login_dev/js/etype_login_dev_page.js
@@ -9,6 +9,9 @@
             $('a[href*="/user/logout"]').click(function () {
                 Cookies.remove("redirectDestination");
             });
+            $("#etype_login_e_edition").once("etypeloginPageBehavior").each(function(e) {
+                $(this).attr("id", "etype_login_e_edition_" + e);
+            });
         }
     };
 })(jQuery, Drupal);
