350e5156ccba68c896fa0fa4fe102efdb5bd0fb4
Charlie Llewellin <charlie@llewellin.net>
2019-08-26 12:30:56 -0500

    ada compliance

--- web/themes/custom/tiempos/js/special_sections.js
+++ web/themes/custom/tiempos/js/special_sections.js
@@ -2,8 +2,10 @@
     "use strict";
     Drupal.behaviors.tiemposSections = {
         attach: function (context, settings) {
-            var text = $(".view-special-sections .view-content > div a:nth-child(2)").html();
-            console.log(text);
+            $(".view-special-sections .view-content div").each(function () {
+                var text = $(this).children("a:nth-child(2)").html();
+                console.log(text);
+            });
         }
     };
 })(jQuery, Drupal);
