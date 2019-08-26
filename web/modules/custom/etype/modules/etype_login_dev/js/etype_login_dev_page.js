c62326b5038d4af5e222162c0c6d5e895d6e1157
Charlie Llewellin <charlie@llewellin.net>
2019-08-26 14:48:20 -0500

    ada compliance

--- web/modules/custom/etype/modules/etype_login_dev/js/etype_login_dev_page.js
+++ web/modules/custom/etype/modules/etype_login_dev/js/etype_login_dev_page.js
@@ -2,6 +2,7 @@
     "use strict";
     Drupal.behaviors.etypeloginPageBehavior = {
         attach: function (context, settings) {
+            var key = 1;
             /* Wildcard to allow for domain in url */
             $('a[href*="/etype-login"]').click(function () {
                 Cookies.set("redirectDestination", window.location.pathname);
@@ -9,9 +10,9 @@
             $('a[href*="/user/logout"]').click(function () {
                 Cookies.remove("redirectDestination");
             });
-            $("#etype_login_e_edition").each(function (key, value) {
-                console.log(key);
+            $("#etype_login_e_edition").each(function () {
                 $(this).attr("id", "etype_login_e_edition_" + key);
+                key += 1;
             });
         }
     };
