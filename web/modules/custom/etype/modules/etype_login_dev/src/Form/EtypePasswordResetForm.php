5b3a6c25f9b35c06268f978f5371782ec09c6380
Charlie Llewellin <charlie@llewellin.net>
2019-08-28 08:57:17 -0500

    Alind's updates to new login and password reset forms

--- web/modules/custom/etype/modules/etype_login_dev/src/Form/EtypePasswordResetForm.php
+++ web/modules/custom/etype/modules/etype_login_dev/src/Form/EtypePasswordResetForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\etype_login_dev\Form;
+namespace Drupal\etype_password_reset\Form;
 
 use Drupal;
 use Drupal\Core\Url;
@@ -55,11 +55,11 @@ class EtypePasswordResetForm extends FormBase {
 
       $form['actions']['#type'] = 'actions';
 
-      $form['actions']['submit'] = [
+      $form['actions']['submit'] = array(
         '#type' => 'submit',
         '#value' => $this->t('Send me my password'),
         '#button_type' => 'primary',
-      ];
+      );
 
     }
     else {
@@ -94,36 +94,24 @@ class EtypePasswordResetForm extends FormBase {
     $username = $form_state->getValue('username');
 
     $pubId = $this->config->get('etype_pub');
+    $pubId = 2;
     $message = "We‘re sorry, we can‘t find an account for that user name at this publication.";
     $success_message = "Your password has been sent to your email adddress.";
 
-    $client = new soapclient('http://etype.wecode4u.com/webservice.asmx/GetPublicationIDByUserName');
-    $param = ['UserName' => $username];
-    $response = $client->GetPublicationID($param);
-    $code = $response->GetPublicationIDResult;
+    $client = new soapclient('http://etype.wecode4u.com/webservice.asmx?WSDL');
+    $param = ['publicationId' => $pubId];
+    $param['username'] = $username;
+    $response = $client->ForgetPassword($param);
+    $code = $response->ForgetPasswordResult;
+
     switch ($code) {
-      case "-9":
+      case "-1":
         Drupal::messenger()->addMessage($message);
-        $form_state->setRebuild();
         break;
 
-      case $pubId:
-        $client = new soapclient('http://etype.wecode4u.com/webservice.asmx/ForgetPassword');
-        $response = $client->ForgetPassword($param);
-        $code = $response->ForgetPasswordResult;
-
-        switch ($code) {
-          case "-1":
-            Drupal::messenger()->addMessage($message);
-            break;
-
-          default:
-            Drupal::messenger()->addMessage($success_message);
-            break;
-        }
-
+      default:
+        Drupal::messenger()->addMessage($success_message);
         break;
     }
   }
-
 }
