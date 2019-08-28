138d444570af7c1babd20286a744d211bc7ea1bc
Charlie Llewellin <charlie@llewellin.net>
2019-08-28 09:02:32 -0500

    Alind's updates to new login and password reset forms

--- web/modules/custom/etype/modules/etype_login_dev/src/Controller/EtypeMyAccountController.php
+++ web/modules/custom/etype/modules/etype_login_dev/src/Controller/EtypeMyAccountController.php
@@ -5,6 +5,7 @@ namespace Drupal\etype_login_dev\Controller;
 use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+
 /**
  * Class EtypeMyAccountController.
  *
@@ -12,8 +13,8 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  */
 class EtypeMyAccountController extends ControllerBase {
 
-   /**
-   * Returns a render-able array for a test page.
+  /**
+   * Returns a render-able array.
    */
   public function content() {
 
@@ -21,8 +22,9 @@ class EtypeMyAccountController extends ControllerBase {
     $config = Drupal::config('etype.adminsettings');
     $pubId = (int) $config->get('etype_pub');
     $build = [
-      '#children' => "<iframe src='http://etype.wecode4u.com/SubscribersProfile.aspx?pid=" . $pubId . "&uid=" . $user_name . "' width='100%' height='600' scrolling='vertical' title='My Account Update Form'></iframe>",
+      '#children' => "<iframe src='https://etype.wecode4u.com/SubscribersProfile.aspx?pid=" . $pubId . "&uid=" . $user_name . "' width='100%' height='600' scrolling='vertical' title='My Account Update Form'></iframe>",
     ];
     return $build;
   }
+
 }
