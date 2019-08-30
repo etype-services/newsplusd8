e7bd3741bc4ae8bce99804d3904a93663aea6d94
Charlie Llewellin <charlie@llewellin.net>
2019-08-30 07:58:11 -0500

    ouray fixes

--- web/themes/custom/tiempos/js/theme.js
+++ web/themes/custom/tiempos/js/theme.js
@@ -30,6 +30,7 @@ function getIEVersion() {
             $(".navbar-burger").once("tiemposBehavior").click(function () {
                 $(".navbar-start").toggleClass("is-really-invisible");
                 $(".user-menu, .region-header-left, .region-header-center").toggleClass("is-hidden-touch");
+                /* User menu might be in top-bar */
                 $("header, .region-top-bar").toggleClass("z-index-fix");
             });
 
