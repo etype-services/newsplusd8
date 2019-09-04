4c311d7a349a0ec162be9fb2e0cb18cdca3c4c82
Charlie Llewellin <charlie@llewellin.net>
2019-09-04 15:14:43 -0400

    ada compliance special sections block

--- web/themes/custom/tiempos/js/special_sections.js
+++ web/themes/custom/tiempos/js/special_sections.js
@@ -5,7 +5,8 @@
             var text;
             /* Add aria-label to special sections image links for ada compliance */
             $(".view-special-sections .view-content div").once("tiemposSections").each(function () {
-                text = $(this).children('a:nth-child(2)').html()
+                text = $(this).children('a:nth-child(2)').html();
+                console.log(text);
                 if (text === "") {
                     text = $(this).children("a:nth-child(1)").attr("title", text);
                 }
