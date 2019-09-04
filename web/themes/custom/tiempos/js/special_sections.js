55485ff71c011fd84e8b25da2bc7557b1ed28f99
Charlie Llewellin <charlie@llewellin.net>
2019-09-04 15:21:43 -0400

    ada compliance special sections block

--- web/themes/custom/tiempos/js/special_sections.js
+++ web/themes/custom/tiempos/js/special_sections.js
@@ -7,6 +7,10 @@
             $(".view-special-sections .view-content div").once("tiemposSections").each(function () {
                 text = $(this).children("a:nth-child(2)").html();
                 if (text === undefined) {
+                    /*
+                     * CNI sites don't show the title.
+                     * it is hidden in the views and added as the "title" for the link.
+                     */
                     text = $(this).children("a:nth-child(1)").attr("title");
                 }
                 $(this).children("a:nth-child(1)").attr("aria-label", text);
