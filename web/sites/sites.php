80d9a188b2fdfb3baf9ed077c7292f1de37606d0
Charlie Llewellin <charlie@llewellin.net>
2019-09-04 14:29:07 -0400

    okcfriday

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -98,6 +98,7 @@ $sites['www.cherokeescout.com'] = 'cherokeescout.etypegoogle7.com';
 $sites['www.grahamstar.com'] = 'grahamstar.etypegoogle7.com';
 $sites['www.lcnok.com'] = 'lcnok.etypegoogle7.com';
 $sites['www.jdcmountaindispatch.com'] = 'jdcmountaindispatch.etypegoogle7.com';
+$sites['www.okcfriday.com'] = 'okcfriday.etypegoogle7.com';
 
 /* jackie */
 $sites['www.bellvilletimes.com'] = "bellvilletimes.etypegoogle11.com";
