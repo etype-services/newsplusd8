220fe6cee3dc9a4549bdeac2419b7df80e8d4f16
Your Name <you@example.com>
2019-09-18 15:25:21 +0000

    update EtypeMyAccountController.php

--- web/modules/custom/etype/modules/etype_login_dev/src/Controller/EtypeMyAccountController.php
+++ web/modules/custom/etype/modules/etype_login_dev/src/Controller/EtypeMyAccountController.php
@@ -22,9 +22,17 @@ class EtypeMyAccountController extends ControllerBase {
     $config = Drupal::config('etype.adminsettings');
     $pubId = (int) $config->get('etype_pub');
     $build = [
-      '#children' => "<iframe src='http://etype.wecode4u.com/subscriber-account?pubId=" . $pubId . "&username=" . $user_name . "' width='100%' height='600' scrolling='vertical' title='My Account Update Form'></iframe>",
+      '#children' => "<script type='text/javascript'>
+                window.addEventListener('message', receiveMessage, false);
+                function receiveMessage(event) {
+                    var ifrmAccountUpdateForm = document.getElementById('ifrmAccountUpdateForm');
+		    if (event.data.docHeight != undefined && event.data.docHeight != 'undefined')
+                        ifrmAccountUpdateForm.height = event.data.docHeight;
+                }
+            </script><iframe id='ifrmAccountUpdateForm' src='http://etype.wecode4u.com/subscriber-account?pubId=" . $pubId . "&username=" . $user_name . "' width='100%' height='860' frameborder='0' scrolling='no'></iframe>",
     ];
     return $build;
   }
 
 }
+
