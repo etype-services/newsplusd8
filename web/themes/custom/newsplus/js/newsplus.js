dd3b3300322ef1272469b45b86d7257d07e0ace1
Charlie Llewellin <charlie@llewellin.net>
2019-09-22 12:09:26 -0400

    ada compliance embedded videos

--- web/themes/custom/newsplus/js/newsplus.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("iframe").each(function () {
-        var attr = $(this).attr("title");
-        console.log(attr);
-        if (typeof attr === typeof undefined) {
-            $(this).attr("title", "Iframe loaded from external site");
-        }
-    });
-});
