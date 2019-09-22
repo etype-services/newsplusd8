14a091d226b63d8f61969dbd4c83fea7ac9a0f6c
Charlie Llewellin <charlie@llewellin.net>
2019-09-22 11:15:54 -0400

    ada compliance

--- web/themes/custom/newsplus/js/newsplus.js
+++ web/themes/custom/newsplus/js/newsplus.js
@@ -4,6 +4,7 @@
         attach: function (context, settings) {
             $("iframe").each(function () {
                 var attr = $(this).attr("title");
+                console.log(attr);
                 if (typeof attr === typeof undefined || attr === false) {
                     $(this).attr("title", "Iframe loaded from external site");
                 }
