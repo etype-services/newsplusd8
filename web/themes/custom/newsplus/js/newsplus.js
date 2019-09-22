e00358f9bcf3390dcf6904eee74c4459cf76097d
Charlie Llewellin <charlie@llewellin.net>
2019-09-22 11:49:43 -0400

    ada compliance revolution slider

--- web/themes/custom/newsplus/js/newsplus.js
+++ web/themes/custom/newsplus/js/newsplus.js
@@ -1,14 +1,9 @@
-(function ($, Drupal) {
-    "use strict";
-    Drupal.behaviors.newsplusIframes = {
-        attach: function (context, settings) {
-            $("iframe").each(function () {
-                var attr = $(this).attr("title");
-                console.log(attr);
-                if (typeof attr === typeof undefined) {
-                    $(this).attr("title", "Iframe loaded from external site");
-                }
-            });
+jQuery(function () {
+    jQuery("iframe").each(function () {
+        var attr = jQuery(this).attr("title");
+        console.log(attr);
+        if (typeof attr === typeof undefined) {
+            jQuery(this).attr("title", "Iframe loaded from external site");
         }
-    };
-})(jQuery, Drupal);
+    });
+});
