0b4ce2ea32bc16875acbf153375382bb3d0da78c
Charlie Llewellin <charlie@llewellin.net>
2019-10-01 11:50:05 -0500

    ADA Compliance

--- web/modules/custom/etype/src/Controller/ADAComplianceController.php
+++ web/modules/custom/etype/src/Controller/ADAComplianceController.php
@@ -2,15 +2,14 @@
 
 namespace Drupal\etype\Controller;
 
-use PDOException;
-use PDO;
+use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Class ADAComplianceController.
  *
  * @package Drupal\etype\Controller
  */
-class ADAComplianceController {
+class ADAComplianceController extends ControllerBase {
 
   /**
    * Class ADAComplianceController.
@@ -18,45 +17,10 @@ class ADAComplianceController {
    * @package Drupal\etype\Controller
    */
   public function adaCompliance() {
-
-    $output = '';
-    $fila = [];
-
-    try {
-      $mbd = new PDO('mysql:host=172.28.1.3:database=66hoursingrants', 'dbadmin', 'BLJPX3pihAJA1AquXpWzGhaGCZxtxxAD',
-      [
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_general_ci',
-        'pdo' => [
-          PDO::MYSQL_ATTR_SSL_KEY => '/etc/mysql/certs/client-key.pem',
-          PDO::MYSQL_ATTR_SSL_CERT => '/etc/mysql/certs/client-cert.pem',
-          PDO::MYSQL_ATTR_SSL_CA => '/etc/mysql/certs/server-ca.pem',
-          PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => FALSE,
-        ],
-      ]
-      );
-      foreach ($mbd->query('show databases') as $fila) {
-        $output .= var_export($fila["Database"], TRUE);
-      }
-    }
-    catch (PDOException $e) {
-      print "¡Error!: " . $e->getMessage() . "<br/>";
-      die();
-    }
-
-    $fixed = array_unique($fila);
-
-    foreach ($fixed as $item) {
-      foreach ($mbd->query("SELECT `body_value` FROM $item.block_content__body WHERE `body_value` REGEXP '<iframe[^>]*>'") as $row) {
-        $output .= var_export($row["body_value"], TRUE);
-      }
-    }
-
-    $build = [
-      '#markup' => $output,
+    return [
+      '#title' => 'Americans with Disabilities Act Compliance Statement',
+      '#theme' => 'ada_compliance',
     ];
-
-    return $build;
   }
 
 }
