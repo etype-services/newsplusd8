db9dcad906e8ee892d9129901c8ce89dc159c43d
Charlie Llewellin <charlie@llewellin.net>
2019-10-01 12:35:42 -0500

    ADA Compliance

--- web/modules/custom/etype/js/etype.js
+++ web/modules/custom/etype/js/etype.js
@@ -9,3 +9,14 @@
         }
     };
 })(jQuery, Drupal);
+
+// ADA Compliance Script
+(function(s, o, g){
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.src = g;
+    a.setAttribute("async","");
+    a.setAttribute("type","text/javascript");
+    a.setAttribute("crossorigin","anonymous");
+    m.parentNode.insertBefore(a, m)
+})(document, "script", "https://adatoolbar.com/wp-json/onlineada/v1/accessibility-toolbar/e687213560c8b");
