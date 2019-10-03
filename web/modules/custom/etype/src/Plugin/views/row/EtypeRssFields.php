d8a6149423b3387bd9b235fc57af000062a19549
Charlie Llewellin <charlie@llewellin.net>
2019-10-03 15:28:26 -0500

    rss feed

--- web/modules/custom/etype/src/Plugin/views/row/EtypeRssFields.php
+++ web/modules/custom/etype/src/Plugin/views/row/EtypeRssFields.php
@@ -31,33 +31,9 @@ class EtypeRssFields extends RssFields {
   public function render($row) {
     $build = parent::render($row);
     $item = $build['#row'];
-    $node = Node::load($row->nid);
-    $item->elements['guid'] = "http://www.etypetest.com/node/" . $row->nid;
+    $item->elements['id'] = $row->nid;
     $build['#row'] = $item;
     return $build;
-  }
-
-  /**
-   * Override buildOptionsForm.
-   *
-   * @param array $form
-   *   The form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   Form State.
-   */
-  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
-    parent::buildOptionsForm($form, $form_state);
-    $initial_labels = ['' => $this->t('- None -')];
-    $view_fields_labels = $this->displayHandler->getFieldLabels();
-    $view_fields_labels = array_merge($initial_labels, $view_fields_labels);
-    $form['image_field'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Image field'),
-      '#description' => $this->t('The field that is going to be used as the RSS item image for each row.'),
-      '#options' => $view_fields_labels,
-      '#default_value' => $this->options['field_image'],
-      '#required' => TRUE,
-    ];
   }
 
 }
