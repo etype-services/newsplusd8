5976084d835b6c5a7a2a942bbdfae3e32d1e7bfb
Charlie Llewellin <charlie@llewellin.net>
2019-10-10 13:12:34 -0500

    adding aria-label to business directory

--- web/themes/custom/tiempos/js/business_directory.js
+++ web/themes/custom/tiempos/js/business_directory.js
@@ -2,6 +2,8 @@
     "use strict";
     Drupal.behaviors.tiemposBusinessDirectory = {
         attach: function (context, settings) {
+            var text;
+
             $("#business-directory").owlCarousel({
                 loop: true,
                 margin: 5,
@@ -18,6 +20,12 @@
                         items: 4
                     },
                 }
+            });
+
+            /* Add aria-label to special sections image links for ada compliance */
+            $("#business-directory .item").once("tiemposSections").each(function () {
+                text = $(this).children("a:nth-child(2)").html();
+                $(this).children("a:nth-child(1)").attr("aria-label", text);
             });
         }
     };
