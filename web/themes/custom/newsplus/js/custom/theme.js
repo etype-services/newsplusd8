3bf5716167aacd98812b92218cc6142a6b272388
Charlie Llewellin <charlie@llewellin.net>
2019-10-11 09:35:43 -0500

    special section aria label

--- web/themes/custom/newsplus/js/custom/theme.js
+++ web/themes/custom/newsplus/js/custom/theme.js
@@ -2,10 +2,14 @@
     "use strict";
     Drupal.behaviors.newsplusTheme = {
         attach: function (context, settings) {
+            var text;
             // Does not add attribute.
             $("#superfish-secondary-menu-toggle, #superfish-main-toggle").attr("aria-label", "Menu Toggle");
             // Hacky workaround for Ponca City News
-            $("#secondary-menu-userlogout > a").attr("href", "/etype-login");
+            text = $("#secondary-menu-userlogout > a").attr("href");
+            if (text === "/user/login") {
+                $("#secondary-menu-userlogout > a").attr("href", "/etype-login");
+            }
         }
     };
 })(jQuery, Drupal);
