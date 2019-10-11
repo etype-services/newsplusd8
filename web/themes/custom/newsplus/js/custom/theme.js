9c3760b7fdfb12865c2a50ff86b759aecb875118
Charlie Llewellin <charlie@llewellin.net>
2019-10-11 09:11:55 -0500

    special section aria label

--- web/themes/custom/newsplus/js/custom/theme.js
+++ web/themes/custom/newsplus/js/custom/theme.js
@@ -2,7 +2,7 @@
     "use strict";
     Drupal.behaviors.newsplusTheme = {
         attach: function (context, settings) {
-            $("#superfish-secondary-menu-toggle, #superfish-main-toggle").attr("aria-label", "Menu Toggle");
+            // $("#superfish-secondary-menu-toggle, #superfish-main-toggle").attr("aria-label", "Menu Toggle");
         }
     };
 })(jQuery, Drupal);
