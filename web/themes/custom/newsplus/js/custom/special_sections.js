355b1d16e858d918f414ab71ccdf2b9d5c9f20a3
Charlie Llewellin <charlie@llewellin.net>
2019-10-11 08:22:31 -0500

    special section aria label

--- web/themes/custom/newsplus/js/custom/special_sections.js
+++ web/themes/custom/newsplus/js/custom/special_sections.js
@@ -4,7 +4,7 @@
         attach: function (context, settings) {
             var text;
             /* Add aria-label to special sections image links for ada compliance */
-            $(".view-special-sections .views-field-field-listing-image img").once("newsplusSections").each(function () {
+            $(".view-special-sections .views-field-field-listing-image a").once("newsplusSections").each(function () {
                 text = $(this).attr("title");
                 $(this).attr("aria-label", text);
             });
