14648a81bf1cc0ab08f33b509abaf0be73021a60
Charlie Llewellin <charlie@llewellin.net>
2019-10-24 18:24:33 -0500

    typography

--- web/themes/custom/tiempos/js/article.js
+++ web/themes/custom/tiempos/js/article.js
@@ -3,16 +3,14 @@
     Drupal.behaviors.tiemposArticle = {
         attach: function (context, settings) {
             var articleCaption;
+            var styles = "caption is-sans-serif is-size-7";
             var flexslider = $("#flexslider-1 > .slides li");
             var len = flexslider.length;
             var img = $(".page-node-type-article article .field--name-body img, .page-node-type-article article .field--type-image img");
-
             img.each(function () {
                 var text = $(this).attr("alt");
                 var width = $(this).width();
-                var styles = "caption is-sans-serif is-size-7";
                 var imgClass = $(this).attr("class");
-
                 if (text !== "undefined") {
                     articleCaption = "<span style=\"width:" + width + "px\" class=\"" + styles + "\">" + text + "</span>";
                     $(this).once("tiemposBehavior").attr("class", "").after(articleCaption).parent().addClass(imgClass);
