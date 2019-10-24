1fd4111410389728c04d44e516349af30cd65e4e
Charlie Llewellin <charlie@llewellin.net>
2019-10-24 11:33:26 -0500

    small embedded images

--- web/themes/custom/tiempos/js/article.js
+++ web/themes/custom/tiempos/js/article.js
@@ -6,16 +6,16 @@
             var flexslider = $("#flexslider-1 > .slides li");
             var len = flexslider.length;
             var img = $(".page-node-type-article article .field--name-body img, .page-node-type-article article .field--type-image img");
+
             img.each(function () {
                 var text = $(this).attr("alt");
                 var styles = "caption is-sans-serif is-size-7";
 
-                if ($(this).hasClass("align-left")) {
-                    $(this).once("tiemposBehavior").wrap("<div class=\"align-left\"></div>");
-                    styles += " align-left";
-                }
-
                 if (text !== "undefined") {
+                    if ($(this).hasClass("align-left")) {
+                        $(this).once("tiemposBehavior").wrap("<div class=\"align-left\"></div>");
+                        styles += " align-left";
+                    }
                     articleCaption = "<p class=\"" + styles + "\">" + text + "</p>";
                     $(this).once("tiemposBehavior").after(articleCaption);
                 }
