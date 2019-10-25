2eb15de5624ba73a48a090fe2655bff122c2b512
Charlie Llewellin <charlie@llewellin.net>
2019-10-25 12:24:40 -0500

    ie css for newsplus

--- web/themes/custom/newsplus/js/custom/theme.js
+++ web/themes/custom/newsplus/js/custom/theme.js
@@ -1,3 +1,17 @@
+function getIEVersion() {
+    var sAgent = window.navigator.userAgent;
+    var Idx = sAgent.indexOf("MSIE");
+    // If IE, return version number.
+    if (Idx > 0) {
+        return parseInt(sAgent.substring(Idx + 5, sAgent.indexOf(".", Idx)));
+    } else if (!!navigator.userAgent.match(/Trident\/7\./)) {
+        // If IE 11 then look for Updated user agent string.
+        return 11;
+    } else {
+        return 0; // It is not IE
+    }
+}
+
 (function ($, Drupal) {
     "use strict";
     Drupal.behaviors.newsplusTheme = {
@@ -9,6 +23,10 @@
             text = $("#secondary-menu-userlogout > a").attr("href");
             if (text === "/user/login") {
                 $("#secondary-menu-userlogout > a").attr("href", "/etype-login");
+            }
+
+            if (getIEVersion() > 0) {
+                $("head").once("newsplusTheme").append('<link rel="stylesheet" type="text/css" href="/themes/custom/newsplus/css/components/ie.css">');
             }
         }
     };
