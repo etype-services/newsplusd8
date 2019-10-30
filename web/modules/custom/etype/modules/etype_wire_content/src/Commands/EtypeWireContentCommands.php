1d62de1b27ef3ec97c53e914008fbc5f2862a7c3
Charlie Llewellin <charlie@llewellin.net>
2019-10-30 07:34:29 -0500

    wire add problems

---
+++ web/modules/custom/etype/modules/etype_wire_content/src/Commands/EtypeWireContentCommands.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\etype_wire_content\Commands;
+
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\etype_wire_content\Controller\WireContentExportController;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ */
+class EtypeWireContentCommands extends DrushCommands {
+
+  /**
+   * Runs Wire Content Export.
+   *
+   * @command etype_wire_content:hello
+   * @aliases wire-export
+   * @usage etype_wire_content:export
+   *   Run command.
+   */
+  public function etypeWireContent() {
+
+    try {
+      (new WireContentExportController)->exportWireContent();
+    }
+    catch (InvalidPluginDefinitionException $e) {
+    }
+    catch (PluginNotFoundException $e) {
+    }
+  }
+
+}
