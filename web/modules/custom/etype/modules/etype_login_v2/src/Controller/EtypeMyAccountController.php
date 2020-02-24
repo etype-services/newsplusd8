fdeec7a7fbee368bb4cc6f1bd22b9ed4a174fa87
Charlie Llewellin <charlie@llewellin.net>
2020-02-24 08:47:27 -0600

    add v2 etype login module

---
+++ web/modules/custom/etype/modules/etype_login_v2/src/Controller/EtypeMyAccountController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\etype_login_v2\Controller;
+
+use Drupal;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class EtypeMyAccountController.
+ *
+ * @package Drupal\etype_login_v2\Controller
+ */
+class EtypeMyAccountController extends ControllerBase {
+
+  /**
+   * Returns a renderable array.
+   */
+  public function content() {
+
+    $user_name = Drupal::currentUser()->getAccountName();
+    $config = Drupal::config('etype.adminsettings');
+    $pubId = (int) $config->get('etype_pub');
+    $build = [
+      '#children' => "<script type='text/javascript'>
+                window.addEventListener('message', receiveMessage, false);
+                function receiveMessage(event) {
+                    var ifrmAccountUpdateForm = document.getElementById('ifrmAccountUpdateForm');
+		    if (event.data.docHeight != undefined && event.data.docHeight != 'undefined')
+                        ifrmAccountUpdateForm.height = event.data.docHeight;
+                }
+            </script><iframe id='ifrmAccountUpdateForm' src='http://etype.wecode4u.com/subscriber-account?pubId=" . $pubId . "&username=" . $user_name . "' width='100%' height='860' frameborder='0' scrolling='no'></iframe>",
+    ];
+    return $build;
+  }
+
+}
+
