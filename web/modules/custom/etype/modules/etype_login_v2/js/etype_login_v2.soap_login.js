5198b952aaacd8643140a703b136c619fdde46c9
Charlie Llewellin <charlie@llewellin.net>
2020-02-27 10:12:22 -0600

    adding soap handler script

--- web/modules/custom/etype/modules/etype_login_v2/js/etype_login_v2.soap_login.js
+++ web/modules/custom/etype/modules/etype_login_v2/js/etype_login_v2.soap_login.js
@@ -4,8 +4,27 @@
         attach: function (context, settings) {
             $(".etype_logged_in").click(function (e) {
                 e.preventDefault();
-                var arr = $(this).attr("href").split("/");
-                console.log(arr[4]);
+                // var arr = $(this).attr("href").split("/");
+                $.soap({
+                    url: 'https://publisher.etype.services/webservice.asmx?op=GenerateUrlForSubscriber',
+
+                    params: {
+                        publicationId: 1,
+                        usename: 'alind'
+                    },
+
+                    success: function (soapResponse) {
+                        // do stuff with soapResponse
+                        // if you want to have the response as JSON use soapResponse.toJSON();
+                        // or soapResponse.toString() to get XML string
+                        // or soapResponse.toXML() to get XML DOM
+                        console.log(soapResponse);
+                    },
+                    error: function (soapResponse) {
+                        // show error
+                        console.log(soapResponse);
+                    }
+                });
             });
         }
     };
