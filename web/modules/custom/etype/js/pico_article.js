0a04c255f9c3ce8e3b49c238485c88b99f91090f
Charlie Llewellin <charlie@llewellin.net>
2020-03-23 09:58:30 -0500

    pico integration

--- web/modules/custom/etype/js/pico_article.js
+++ web/modules/custom/etype/js/pico_article.js
@@ -3,14 +3,15 @@
     Drupal.behaviors.pico = {
         attach: function (context, settings) {
             var nid = drupalSettings.nid;
+            var picoCategories = drupalSettings.picoCategories;
             var pageInfo = {
                 article: true,
                 post_id: nid,
                 post_type: 'article',
-                //taxonomies: {
+                taxonomies: {
                 //    tags: [...],
-                //    categories: [...],
-                //},
+                    categories: picoCategories,
+                },
                 url: window.location.href
             };
 
