15ab99fed40ac5e4a501922b79bc2b1ff1fb2fd4
Charlie Llewellin <charlie@llewellin.net>
2020-03-26 13:07:44 -0500

    pico integration

--- web/modules/custom/etype/js/pico_article.js
+++ web/modules/custom/etype/js/pico_article.js
@@ -2,22 +2,6 @@
     'use strict';
     Drupal.behaviors.picoArticle = {
         attach: function (context, settings) {
-            var picoPublisherId = drupalSettings.picoPublisherId;
-            console.log(picoPublisherId);
-            (function (p, i, c, o) {
-                var n = new Event('pico-init');
-                i[p] = i[p] ||
-                    function () {(i[p].queue = i[p].queue || []).push(arguments)}, i.document.addEventListener(
-                    'pico-init', function (e) {
-                        var t = i.Pico.getInstance(e, {publisherId: o, picoInit: n}, i)
-                        t.handleQueueItems(i[p].queue), i[p] = function () {return t.handleQueueItems([arguments])}
-                    }, !1);
-                var e = i.document.createElement('script'), t = i.document.getElementsByTagName('script')[0];
-                e.async = 1, e.src = c, e.onload = function (e) {
-                    return i.Pico.getInstance(e, {publisherId: o, picoInit: n}, i)
-                }, t.parentNode.insertBefore(e, t)
-            })('pico', window, "https://widget.pico.tools/wrapper.min.js", picoPublisherId);
-
             var nid = settings.nid;
             var picoCategories = settings.picoCategories;
             var pageInfo = {
