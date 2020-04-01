ce64a2bd84332da878304f76ded8247a2bd87f93
Charlie Llewellin <charlie@llewellin.net>
2020-04-01 12:09:44 -0500

    rss fields for mailchimp

--- web/modules/custom/etype/src/Plugin/views/row/EtypeRssFields.php
+++ web/modules/custom/etype/src/Plugin/views/row/EtypeRssFields.php
@@ -40,9 +40,11 @@ class EtypeRssFields extends RssFields {
     // Set the title and created date.
     $item->title = $node->getTitle();
     var_dump($node->created->value);
-    exit;
     // $date = date_create($node->created->value());
-    //$date = date("Y-m-d\TH:i:s\Z", $node->created->value());
+    $date = date("Y-m-d\TH:i:s\Z", $node->created->value());
+    var_dump ($date);
+    exit;
+
     //dpm($date);
     //$item->elements[] = [
     //  'key' => 'pubDate',
