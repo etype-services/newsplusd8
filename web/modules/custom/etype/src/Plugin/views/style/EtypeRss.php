223318e7021856ebae6d3ed48061534970d86e80
Charlie Llewellin <charlie@llewellin.net>
2020-04-04 10:44:55 -0500

    mailchimp feed

---
+++ web/modules/custom/etype/src/Plugin/views/style/EtypeRss.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\etype\Plugin\views\style;
+
+/**
+ * This is the eType style plugin to render an RSS feed.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "etype_rss",
+ *   title = @Translation("eType RSS Feed"),
+ *   help = @Translation("Generates an RSS feed from a view."),
+ *   theme = "views_view_rss",
+ *   display_types = {"feed"}
+ * )
+ */
+class EtypeRss extends Rss {
+
+  /**
+   * Override Views Rss styles.
+   *
+   * @return array
+   *   Return Build.
+   */
+  public function render() {
+
+    if (empty($this->view->rowPlugin)) {
+      trigger_error('Drupal\views\Plugin\views\style\Rss: Missing row plugin', E_WARNING);
+      return [];
+    }
+    $rows = [];
+
+    // This will be filled in by the row plugin and is used later on in the
+    // theming output.
+    $this->namespaces = ['xmlns:dc' => 'http://purl.org/dc/elements/1.1/'];
+
+    // Fetch any additional elements for the channel and merge in their
+    // namespaces.
+    $this->channel_elements = $this->getChannelElements();
+    foreach ($this->channel_elements as $element) {
+      if (isset($element['namespace'])) {
+        $this->namespaces = array_merge($this->namespaces, $element['namespace']);
+      }
+    }
+
+    foreach ($this->view->result as $row_index => $row) {
+      $this->view->row_index = $row_index;
+      $rows[] = $this->view->rowPlugin->render($row);
+    }
+
+    $build = [
+      '#theme' => $this->themeFunctions(),
+      '#view' => $this->view,
+      '#options' => $this->options,
+      '#rows' => $rows,
+    ];
+    unset($this->view->row_index);
+    return $build;
+  }
+
+}
