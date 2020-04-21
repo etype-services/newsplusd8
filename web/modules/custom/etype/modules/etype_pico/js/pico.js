e419ce6894c76aeaaf571cacf0e6d155ab3becd8
Charlie Llewellin <charlie@llewellin.net>
2020-04-21 17:05:33 -0500

    pico integration

--- web/modules/custom/etype/modules/etype_pico/js/pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/pico.js
@@ -3,7 +3,6 @@
     Drupal.behaviors.picoBehavior = {
         attach: function (context, settings) {
             $('.user-menu a[href="/"]').addClass('PicoRule PicoSignal');
-            $('.user-menu a[data-pico-status="registered"]').addClass('is-really-invisible');
         }
     };
 })(jQuery, Drupal);
