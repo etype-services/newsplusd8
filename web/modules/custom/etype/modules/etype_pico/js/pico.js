1464079fd4dd806d55034e947e348fbce618215a
Charlie Llewellin <charlie@llewellin.net>
2020-04-21 17:19:05 -0500

    pico integration

--- web/modules/custom/etype/modules/etype_pico/js/pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/pico.js
@@ -2,7 +2,8 @@
     "use strict";
     Drupal.behaviors.picoBehavior = {
         attach: function (context, settings) {
-            $('.user-menu a[href="/"]').addClass('PicoPlan PicoSignal');
+            $('.user-menu a[href="/login"]').addClass('PicoRule PicoSignal');
+            $('.user-menu a[href="/subscribe"]').addClass('PicoPlan PicoSignal');
         }
     };
 })(jQuery, Drupal);
