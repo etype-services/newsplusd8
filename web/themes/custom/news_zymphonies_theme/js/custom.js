9a6d209eaae0fea7eb3f39de8e2121d8a59317e8
Charlie Llewellin <charlie@llewellin.net>
2020-04-30 11:09:09 -0500

    font-awesome
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/news_zymphonies_theme/js/custom.js
+++ web/themes/custom/news_zymphonies_theme/js/custom.js
@@ -1,4 +1,4 @@
-/* --------------------------------------------- 
+/* ---------------------------------------------
 * Filename:     custom.js
 * Version:      1.0.0 (2017-02-12)
 * Website:      http://www.zymphonies.com
@@ -7,67 +7,58 @@
                 info@zymphonies.com
 -----------------------------------------------*/
 
-function clients_owl(){
-	jQuery('.field--name-field-clients-logo').owlCarousel({
-		items: 2,
-		margin:10,
-		dots: true,
-		autoPlay: 3000,
-		navigation : true,
-		responsive : {
-			500:{ items: 2, dots: true, navigation : true },
-			700:{ items: 3, dots: true, navigation : true },
-			900:{ items: 5, dots: true, navigation : true }
-		}
-	});
-}
-
-function service_owl(){
-	jQuery('.field--name-field-service').owlCarousel({
-		items: 1,
-		margin:10,
-		dots: true,
-		autoPlay: 3000,
-		navigation : true,
-		responsive : {
-			500:{ items: 1, dots: true, navigation : true },
-			700:{ items: 2, dots: true, navigation : true },
-			900:{ items: 4, dots: true, navigation : true }
-		}
-	});
-}
-
-function theme_menu(){
-
-	//Main menu
-	jQuery('#main-menu').smartmenus();
-	
-	//Mobile menu toggle
-	jQuery('.navbar-toggle').click(function(){
-		jQuery('.region-primary-menu').slideToggle();
-	});
-
-	//Mobile dropdown menu
-	if ( jQuery(window).width() < 767) {
-		jQuery(".region-primary-menu li a:not(.has-submenu)").click(function () {
-			jQuery('.region-primary-menu').hide();
-	    });
-	}
-
-}
-
-function theme_home(){
-	
-	//flexslider
-	jQuery('.flexslider').flexslider({
-    	animation: "slide"	
+function clients_owl () {
+    jQuery('.field--name-field-clients-logo').owlCarousel({
+        items: 2, margin: 10, dots: true, autoPlay: 3000, navigation: true, responsive: {
+            500: {items: 2, dots: true, navigation: true},
+            700: {items: 3, dots: true, navigation: true},
+            900: {items: 5, dots: true, navigation: true},
+        },
     });
-
 }
 
-jQuery(document).ready(function($){
-	clients_owl();
-	service_owl();
-	theme_menu();
-	theme_home();
-});
+function service_owl () {
+    jQuery('.field--name-field-service').owlCarousel({
+        items: 1, margin: 10, dots: true, autoPlay: 3000, navigation: true, responsive: {
+            500: {items: 1, dots: true, navigation: true},
+            700: {items: 2, dots: true, navigation: true},
+            900: {items: 4, dots: true, navigation: true},
+        },
+    });
+}
+
+function theme_menu () {
+
+    //Main menu
+    jQuery('#main-menu').smartmenus();
+
+    //Mobile menu toggle
+    jQuery('.navbar-toggle').click(function () {
+        jQuery('.region-primary-menu').slideToggle();
+    })
+
+    //Mobile dropdown menu
+    if (jQuery(window).width() < 767) {
+        jQuery('.region-primary-menu li a:not(.has-submenu)').click(function () {
+            jQuery('.region-primary-menu').hide();
+        });
+    }
+}
+
+function theme_home () {
+    //flexslider
+    jQuery('.flexslider').flexslider({
+        animation: 'slide',
+    });
+}
+
+function search_form() {
+    jQuery('.block-searchform #edit-submit').after("<i class=\"fas fa-search\"></i>");
+}
+
+jQuery(document).ready(function ($) {
+    clients_owl();
+    service_owl();
+    theme_menu();
+    theme_home();
+})
