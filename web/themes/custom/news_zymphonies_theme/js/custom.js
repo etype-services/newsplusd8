21c37b77cc5e1d41098fc767d0f223cca7586038
Charlie Llewellin <charlie@llewellin.net>
2020-05-07 11:58:06 -0500

    zymphonies css fixes
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/news_zymphonies_theme/js/custom.js
+++ web/themes/custom/news_zymphonies_theme/js/custom.js
@@ -25,20 +25,11 @@
 
             var themeMenu = function() {
 
-                // Main menu
-                $('#main-menu').smartmenus();
-
                 // Mobile menu toggle
                 $('.navbar-toggle').once("newszymphoniesBehavior").click(function () {
-                    $('.region-primary-menu, .superfish-main-accordion').slideToggle();
+                    $('.superfish-main-accordion').slideToggle();
                 });
 
-                // Mobile dropdown menu
-                if ($(window).width() < 767) {
-                    $('.region-primary-menu li a:not(.has-submenu)').click(function () {
-                        $('.region-primary-menu').hide();
-                    });
-                }
             };
 
             // clientsOwl();
