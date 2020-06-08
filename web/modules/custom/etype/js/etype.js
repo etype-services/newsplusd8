f51a7622fa598b9af3854efbe15a6cdc5cc143b8
Charlie Llewellin <charlie@llewellin.net>
2020-06-08 15:54:20 -0500

    V2 login fixes
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/js/etype.js
+++ web/modules/custom/etype/js/etype.js
@@ -2,7 +2,7 @@
     "use strict";
     Drupal.behaviors.etypeAuthorBehavior = {
         attach: function (context, settings) {
-            var author = drupalSettings.etype.author;
+            let author = drupalSettings.etype.author;
             if (author.length > 0) {
                 $("#edit-uid-0-target-id").val(author);
             }
