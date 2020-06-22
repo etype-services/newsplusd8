55cc2c3f68c751c44261398296e9eb8441510f08
Charlie Llewellin <charlie@llewellin.net>
2020-06-22 12:34:18 -0500

    bulma missing?
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/tiempos/js/front_page_sections.js
+++ web/themes/custom/tiempos/js/front_page_sections.js
@@ -11,12 +11,12 @@
                     let maxHeight;
                     $("img", obj).each(function () {
                         height = $(this).height();
-                        console.log('height: ' + height);
+                        // console.log('height: ' + height);
                         heights.push(parseInt(height));
                     });
                     // console.log(heights);
                     maxHeight = Math.min.apply(Math, heights);
-                    console.log('maxHeight: ' + maxHeight);
+                    // console.log('maxHeight: ' + maxHeight);
                     if (maxHeight > 0) {
                         $(".image-container img", obj).css({
                             height: maxHeight,
@@ -26,7 +26,7 @@
                 });
             };
             $(".region-content").imagesLoaded().done(function(instance) {
-                console.log('all images successfully loaded');
+                // console.log('All images successfully loaded');
                 imageFixer();
             });
             $(window).resize(function () {
