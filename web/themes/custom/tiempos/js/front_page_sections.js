11bf81fe5b3d26bf48be92330462a5a75f1fa501
Charlie Llewellin <charlie@llewellin.net>
2020-06-22 12:20:21 -0500

    bulma missing?
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/tiempos/js/front_page_sections.js
+++ web/themes/custom/tiempos/js/front_page_sections.js
@@ -25,7 +25,7 @@
                     }
                 });
             };
-            $(".region-content").imagesLoaded().done( function( instance ) {
+            $(".region-content").imagesLoaded().done(function(instance) {
                 console.log('all images successfully loaded');
                 imageFixer();
             });
