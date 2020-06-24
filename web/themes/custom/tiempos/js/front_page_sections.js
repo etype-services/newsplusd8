5943a68fafca8d8b3e8ec05a4b0bcf5dad441175
Charlie Llewellin <charlie@llewellin.net>
2020-06-24 07:23:14 -0500

    front page sections
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/tiempos/js/front_page_sections.js
+++ web/themes/custom/tiempos/js/front_page_sections.js
@@ -1,39 +1,45 @@
 /* jshint esversion: 6 */
+
+function showImages () {
+    $(".image-container").css({
+        background: "none"
+    });
+    $(".image-container img").css({
+        visibility: "visible"
+    });
+}
+
+function imageFixer () {
+    $(".front-page-sections").each(function () {
+        let obj = this;
+        let height;
+        let heights = [];
+        let maxHeight;
+        $("img", obj).each(function () {
+            height = $(this).height();
+            // console.log('height: ' + height);
+            heights.push(parseInt(height));
+        });
+        // console.log(heights);
+        maxHeight = Math.min.apply(Math, heights);
+        // console.log('maxHeight: ' + maxHeight);
+        if (maxHeight > 0) {
+            $(".image-container img", obj).css({
+                height: maxHeight,
+                visibility: "visible"
+            });
+        }
+        $(".image-container").css({
+            background: "none"
+        });
+    });
+};
+
 (function ($, Drupal) {
     "use strict";
     Drupal.behaviors.tiemposFrontPageSections = {
         attach: function (context, settings) {
-            let showImages = function () {
-                $(".image-container img").css({
-                    visibility: "visible"
-                });
-            };
 
-            let imageFixer = function () {
-                $(".front-page-sections").each(function () {
-                    let obj = this;
-                    let height;
-                    let heights = [];
-                    let maxHeight;
-                    $("img", obj).each(function () {
-                        height = $(this).height();
-                        // console.log('height: ' + height);
-                        heights.push(parseInt(height));
-                    });
-                    // console.log(heights);
-                    maxHeight = Math.min.apply(Math, heights);
-                    // console.log('maxHeight: ' + maxHeight);
-                    if (maxHeight > 0) {
-                        $(".image-container img", obj).css({
-                            height: maxHeight,
-                            visibility: "visible"
-                        });
-                    }
-                    $(".image-container").css({
-                        background: "none"
-                    });
-                });
-            };
             $(".front-page-sections").imagesLoaded().done(function(instance) {
                 // console.log('All images successfully loaded');
                 imageFixer();
