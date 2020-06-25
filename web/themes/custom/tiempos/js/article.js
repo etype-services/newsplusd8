ff8699c325bc3ff5ebc0b5ac1f25464eec95067f
Charlie Llewellin <charlie@llewellin.net>
2020-06-25 13:40:47 -0500

    Flexslider CSS
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/tiempos/js/article.js
+++ web/themes/custom/tiempos/js/article.js
@@ -7,17 +7,18 @@
             let styles = "caption is-sans-serif is-size-7";
             let flexslider = $("#flexslider-1 > .slides li");
             let len = flexslider.length;
-
             let img = $(".page-node-type-article article .field--name-body img, .page-node-type-article article .field--type-image img");
-            img.each(function () {
-                let text = $(this).attr("alt");
-                let width = $(this).width();
-                let imgClass = $(this).attr("class");
-                if (text !== "undefined") {
-                    articleCaption = "<span style=\"width:" + width + "px\" class=\"" + styles + "\">" + text + "</span>";
-                    $(this).once("tiemposBehavior").attr("class", "").after(articleCaption).parent().addClass(imgClass).css("width", width + "px");
-                }
-            });
+            if ($('#flex-caption').length === 0) {
+                img.each(function () {
+                    let text = $(this).attr("alt");
+                    let width = $(this).width();
+                    let imgClass = $(this).attr("class");
+                    if (text !== "undefined") {
+                        articleCaption = "<span style=\"width:" + width + "px\" class=\"" + styles + "\">" + text + "</span>";
+                        $(this).once("tiemposBehavior").attr("class", "").after(articleCaption).parent().addClass(imgClass).css("width", width + "px");
+                    }
+                });
+            }
 
             if (len > 1) {
                 $("#flexslider-1").css("margin-bottom", "3rem");
