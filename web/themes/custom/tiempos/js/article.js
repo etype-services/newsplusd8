5633841be362892259646722714186792a6161ee
Charlie Llewellin <charlie@llewellin.net>
2020-06-25 10:42:12 -0500

    pawnee and tecumseh social icons
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/themes/custom/tiempos/js/article.js
+++ web/themes/custom/tiempos/js/article.js
@@ -1,17 +1,18 @@
+/* jshint esversion: 6 */
 (function ($, Drupal) {
     "use strict";
     Drupal.behaviors.tiemposArticle = {
         attach: function (context, settings) {
-            var articleCaption;
-            var styles = "caption is-sans-serif is-size-7";
-            var flexslider = $("#flexslider-1 > .slides li");
-            var len = flexslider.length;
+            let articleCaption;
+            let styles = "caption is-sans-serif is-size-7";
+            let flexslider = $("#flexslider-1 > .slides li");
+            let len = flexslider.length;
 
-            var img = $(".page-node-type-article article .field--name-body img, .page-node-type-article article .field--type-image img");
+            let img = $(".page-node-type-article article .field--name-body img, .page-node-type-article article .field--type-image img");
             img.each(function () {
-                var text = $(this).attr("alt");
-                var width = $(this).width();
-                var imgClass = $(this).attr("class");
+                let text = $(this).attr("alt");
+                let width = $(this).width();
+                let imgClass = $(this).attr("class");
                 if (text !== "undefined") {
                     articleCaption = "<span style=\"width:" + width + "px\" class=\"" + styles + "\">" + text + "</span>";
                     $(this).once("tiemposBehavior").attr("class", "").after(articleCaption).parent().addClass(imgClass).css("width", width + "px");
