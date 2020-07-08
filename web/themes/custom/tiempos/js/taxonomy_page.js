89b55e1ef5ab827a90ca88852938df13bf02af1b
Charlie Llewellin <charlie@llewellin.net>
2020-07-08 13:40:12 -0500

    js for taxonomy pages

---
+++ web/themes/custom/tiempos/js/taxonomy_page.js
@@ -0,0 +1,54 @@
+/* jshint esversion: 6 */
+(function ($, Drupal) {
+  "use strict";
+  Drupal.behaviors.tiemposTaxonomyPage = {
+    attach: function (context, settings) {
+
+      let showImages = function () {
+        $(".image-container").css({
+          background: "none"
+        });
+        $(".image-container img").css({
+          visibility: "visible"
+        });
+      };
+
+      let imageFixer = function () {
+        $(".view-content").each(function () {
+          let obj = this;
+          let height;
+          let heights = [];
+          let maxHeight;
+          $("img", obj).each(function () {
+            height = $(this).height();
+            // console.log('height: ' + height);
+            heights.push(parseInt(height));
+          });
+          // console.log(heights);
+          maxHeight = Math.min.apply(Math, heights);
+          // console.log('maxHeight: ' + maxHeight);
+          if (maxHeight > 0) {
+            $(".image-container img", obj).css({
+              height: maxHeight,
+              visibility: "visible"
+            });
+          }
+          $(".image-container").css({
+            background: "none"
+          });
+        });
+      };
+      $(".front-page-sections").imagesLoaded().done(function(instance) {
+        // console.log('All images successfully loaded');
+        imageFixer();
+      });
+
+      $(window).resize(function () {
+        imageFixer();
+      });
+
+      // Make sure images show, as sometimes buggy ad scripts seem to prevent the script completing.
+      setTimeout(showImages, 1500);
+    }
+  };
+})(jQuery, Drupal);
