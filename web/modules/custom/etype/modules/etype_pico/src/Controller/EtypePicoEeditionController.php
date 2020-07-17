86392c7998abe3e7a8b4e8515b1e1ccefafbc17c
Charlie Llewellin <charlie@llewellin.net>
2020-07-17 17:19:22 -0500

    Pico - eType Services authentication token
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

---
+++ web/modules/custom/etype/modules/etype_pico/src/Controller/EtypePicoEeditionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\etype_pico\Controller;
+
+use Drupal;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+/**
+ * Class EtypePicoEeditionController.
+ *
+ * @package Drupal\etype_pico\Controller
+ */
+class EtypePicoEeditionController extends ControllerBase {
+
+  /**
+   * Returns a verified url for the subscriber with an access token.
+   *
+   * This is used to open the etypeservices page for the paper.
+   *
+   * @return string
+   *   The url with the token.
+   */
+  public function getToken() {
+    $config = Drupal::config('etype.adminsettings');
+    $pubId = (int) $config->get('etype_pub');
+    $client = new soapclient('https://publisher.etype.services/webservice.asmx?WSDL');
+    $params = [
+      'publicationId' => $pubId,
+      'username' => 'Pico',
+    ];
+    $data = $client->GenerateUrlForSubscriber($params);
+    return $data->GenerateUrlForSubscriberResult;
+  }
+
+  /**
+   * Returns a TrustedRedirectResponse.
+   *
+   * @param int $pubId
+   *   the Id of the publication.
+   *
+   * @return \Drupal\Core\Routing\TrustedRedirectResponse
+   *   Redirect to external uri.
+   */
+  public function gotoEedition($pubId = NULL) {
+    $url = $this->getToken();
+    $response = new TrustedRedirectResponse($url);
+    /* We do not want the response cached */
+    $cacheable_metadata = new CacheableMetadata();
+    $cacheable_metadata->setCacheMaxAge(0);
+    $response->addCacheableDependency($cacheable_metadata);
+    return $response;
+  }
+
+}
