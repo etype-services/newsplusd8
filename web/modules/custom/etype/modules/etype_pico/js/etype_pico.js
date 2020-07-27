baf414b2b585cfd1feee9acbc1308e3822d38395
Charlie Llewellin <charlie@llewellin.net>
2020-07-27 18:49:30 -0500

    Pico check if user logged in
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -4,11 +4,6 @@
         attach: function (context, settings) {
             $('.user-menu a[href="/login"]').addClass('PicoRule PicoSignal');
             $('.user-menu a[href="/subscribe"]').addClass('PicoPlan PicoSignal');
-
-            /*$('a[href="/e-edition"]')once("picoBehavior").on('click', function () {
-                console.log($(this).attr('href'));
-                console.log(window.Pico.user);
-            });*/
         }
     };
 })(jQuery, Drupal);
