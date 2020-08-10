fd3bd749f1c13f4f657a4cbdc30266bbaca50bb9
Charlie Llewellin <charlie@llewellin.net>
2020-08-10 18:25:08 -0500

    Reconfigure to Pico module to add validateSubscriber step
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -1,14 +1,11 @@
-(function ($, Drupal) {
-    "use strict";
-    Drupal.behaviors.picoAuthBehavior = {
-        let encryption = new Encryption();
-        let redirect = encryption.decrypt('{{ url }}', '{{ salt }}');
-        window.addEventListener('pico.loaded', function() {
-            console.log(window.Pico.user.verified);
-            console.log(redirect);
-            if (window.Pico.user.verified == true) {
-                window.location.replace(redirect);
-            }
-        });
-    }
-})(jQuery, Drupal);
+$(document).ready(function(){
+    const encryption = new Encryption();
+    const redirect = encryption.decrypt('{{ url }}', '{{ salt }}');
+    window.addEventListener('pico.loaded', function() {
+        console.log(window.Pico.user.verified);
+        console.log(redirect);
+        if (window.Pico.user.verified == true) {
+            window.location.replace(redirect);
+        }
+    });
+});
