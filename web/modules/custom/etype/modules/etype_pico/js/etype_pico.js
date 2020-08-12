03a8cd9a89f7ce8ac27fdf96ae3af057cc36724c
Charlie Llewellin <charlie@llewellin.net>
2020-08-12 13:53:13 -0500

    testing on etypetest.com
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -2,8 +2,8 @@
     "use strict";
     Drupal.behaviors.picoBehavior = {
         attach: function (context, settings) {
-            $('.user-menu a[href="/login"]').addClass('PicoRule PicoSignal');
-            $('.user-menu a[href="/subscribe"]').addClass('PicoPlan PicoSignal');
+            $('.user-menu a[href="/pico-login"]').addClass('PicoRule PicoSignal');
+            $('.user-menu a[href="/pico-subscribe"]').addClass('PicoPlan PicoSignal');
         }
     };
 })(jQuery, Drupal);
--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -3,13 +3,12 @@
     Drupal.behaviors.picoAuthBehavior = {
         attach: function (context, settings){
             // console.log(window.Pico.user.verified);
-            alert(drupalSettings.toknizdUrl);
-            alert('yes');
+            console.log(drupalSettings.toknizdUrl);
             //window.addEventListener('pico.loaded', function() {
-                //if (window.Pico.user.verified == true) {
-                //    window.location.replace(redirect);
+                //if (window.Pico.user.verified === true) {
+                    window.location.replace(drupalSettings.toknizdUrl);
                 //}
             //});
         }
-    };
+    }
 })(jQuery, Drupal, drupalSettings);
