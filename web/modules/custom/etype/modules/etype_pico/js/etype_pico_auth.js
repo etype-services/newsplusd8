0fad2be70486c8644a27afcf20957ddf2a214f6f
Charlie Llewellin <charlie@llewellin.net>
2020-08-12 15:54:08 -0500

    testing on etypetest.com
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -2,8 +2,7 @@
     "use strict";
     Drupal.behaviors.picoAuthBehavior = {
         attach: function (context, settings){
-            // console.log(window.Pico.user.verified);
-            console.log(drupalSettings.toknizdUrl);
+            // console.log(drupalSettings.toknizdUrl);
             window.addEventListener('pico.loaded', function() {
                 if (window.Pico.user.verified === true) {
                     window.location.replace(drupalSettings.toknizdUrl);
