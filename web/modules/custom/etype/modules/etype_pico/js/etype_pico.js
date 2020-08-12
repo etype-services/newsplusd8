76ac1e5b85f1022c5023ce60b94fcc4a76d81302
Charlie Llewellin <charlie@llewellin.net>
2020-08-12 17:46:38 -0500

    testing on etypetest.com
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -4,6 +4,9 @@
         attach: function (context, settings) {
             $('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
             $('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
+            $('.user-menu a[href="/pico-login"], .user-menu a[href="/pico-subscribe"]').on('click', function (event){
+                event.preventDefault();
+            });
         }
     };
 })(jQuery, Drupal);
