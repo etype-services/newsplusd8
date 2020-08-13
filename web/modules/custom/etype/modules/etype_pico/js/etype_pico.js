47c75f97be95bda7f138b041aa314323f61c3f07
Charlie Llewellin <charlie@llewellin.net>
2020-08-13 16:24:22 -0500

    merge pico to master
    Signed-off-by: Charlie Llewellin <charlie@llewellin.net>

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -2,8 +2,11 @@
     "use strict";
     Drupal.behaviors.picoBehavior = {
         attach: function (context, settings) {
-            $('.user-menu a[href="/login"]').addClass('PicoRule PicoSignal');
-            $('.user-menu a[href="/subscribe"]').addClass('PicoPlan PicoSignal');
+            $('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
+            $('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
+            $('.user-menu a[href="/pico-login"], .user-menu a[href="/pico-subscribe"]').on('click', function (event){
+                event.preventDefault();
+            });
         }
     };
 })(jQuery, Drupal);
--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-    const encryption = new Encryption();
-    const redirect = encryption.decrypt('{{ url }}', '{{ salt }}');
-    window.addEventListener('pico.loaded', function() {
-        console.log(window.Pico.user.verified);
-        console.log(redirect);
-        if (window.Pico.user.verified == true) {
-            window.location.replace(redirect);
+(function ($, Drupal, drupalSettings) {
+    "use strict";
+    Drupal.behaviors.picoAuthBehavior = {
+        attach: function (context, settings){
+            // console.log(drupalSettings.toknizdUrl);
+            window.addEventListener('pico.loaded', function() {
+                if (window.Pico.user.verified === true) {
+                    window.location.replace(drupalSettings.toknizdUrl);
+                }
+            });
         }
-    });
-});
+    };
+})(jQuery, Drupal, drupalSettings);
