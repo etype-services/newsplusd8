a564998783b12f673bb2c06a8d3ce632d3ffb22b
Charlie Llewellin <charlie@llewellin.net>
2020-11-13 12:05:38 -0600

    Subscriber Import

--- web/modules/custom/etype/modules/etype_subscribers/src/Form/EtypeSubscribersImportForm.php
+++ web/modules/custom/etype/modules/etype_subscribers/src/Form/EtypeSubscribersImportForm.php
@@ -2,23 +2,33 @@
 
 namespace Drupal\etype_subscribers\Form;
 
-use Drupal;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class EtypeSubscribersImportForm.
- *
- * @package Drupal\etype_subscribers\Form
  */
 class EtypeSubscribersImportForm extends FormBase {
 
+  /**
+   * Get Form Id.
+   *
+   * @return string
+   *   Form Id
+   */
   public function getFormId() {
     return 'etype_subscribers_import_form';
   }
 
+  /**
+   * Build the Form.
+   *
+   * @param array $form
+   * @param FormStateInterface $form_state
+   * @return array
+   *   Form
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
     $form['subscribers'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('CSV file of Subscribers'),
@@ -28,20 +38,24 @@ class EtypeSubscribersImportForm extends FormBase {
       ],
     ];
 
-    $form['submit'] = array(
+    $form['submit'] = [
       '#type' => 'submit',
       '#value' => 'Import Subscribers',
-    );
+    ];
 
     return $form;
   }
 
+  /**
+   * Validate Form.
+   *
+   * @param array $form
+   *   Form.
+   * @param FormStateInterface $form_state
+   */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
   }
-
 }
