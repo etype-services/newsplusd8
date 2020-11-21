028e4034c2983578c096f234cff145e4f9af9899
Charlie Llewellin <charlie@llewellin.net>
2020-11-21 10:11:00 -0600

    Adding eType Commerce Module

--- web/modules/custom/etype/modules/etype_paywall/js/etype_paywall.js
+++ web/modules/custom/etype/modules/etype_paywall/js/etype_paywall.js
@@ -14,20 +14,20 @@ let newTotal, etype_paywall_init = function () {
 etype_paywall_init();
 
 (function ($, Drupal) {
-    "use strict";
     Drupal.behaviors.etypePaywallPageBehavior = {
         attach: function (context, settings) {
-            let expiresNumber = parseInt(drupalSettings.etype_paywall.etype_paywall.expiresNumber);
-            let freeNumber = parseInt(drupalSettings.etype_paywall.etype_paywall.freeNumber);
-            let subLink = drupalSettings.etype_paywall.etype_paywall.subLink;
-            if (newTotal > freeNumber) {
-              let message = "You‘ve read all your free articles in this " + expiresNumber + " day period. Please <a href=\"" + subLink + "\">subscribe</a> to read more."
-              $(".node__content .field--name-body").once("etypePaywallPageBehavior").addClass("paywall_blocked").html(message);
-              $("#block-paywallblock").html(message);
-            } else {
-              $("#block-paywallblock").html("<p>You have read " + newTotal + " of " + freeNumber + " free articles available in this " + expiresNumber + " day period.</p>");
-            }
-            Cookies.set("paywallViewed", newTotal, {expires: expiresNumber});
+          "use strict";
+          let expiresNumber = parseInt(drupalSettings.etype_paywall.etype_paywall.expiresNumber);
+          let freeNumber = parseInt(drupalSettings.etype_paywall.etype_paywall.freeNumber);
+          let subLink = drupalSettings.etype_paywall.etype_paywall.subLink;
+          if (newTotal > freeNumber) {
+            let message = "You‘ve read all your free articles in this " + expiresNumber + " day period. Please <a href=\"" + subLink + "\">subscribe</a> to read more.";
+            $(".node__content .field--name-body").once("etypePaywallPageBehavior").addClass("paywall_blocked").html(message);
+            $("#block-paywallblock").html(message);
+          } else {
+            $("#block-paywallblock").html("<p>You have read " + newTotal + " of " + freeNumber + " free articles available in this " + expiresNumber + " day period.</p>");
+          }
+          Cookies.set("paywallViewed", newTotal, {expires: expiresNumber});
         }
     };
 })(jQuery, Drupal);
