1bc05ff7f78adebbf2c6a2a4df1c70f04a68e979
Charlie Llewellin <charlie@llewellin.net>
2020-11-22 13:16:39 -0600

    Adding eType Commerce Module

--- web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
+++ web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
@@ -37,6 +37,8 @@ class OrderEventSubscriber implements EventSubscriberInterface {
     $order = $event->getEntity();
     $customer_id = $order->getCustomerId();
     $user = User::load($customer_id);
+    $username = $user->getUsername();
+    $message = '';
     /* Extend subscription by 1 year from today if Sub Expiry is in past, or 1 year from Sub Expiry. */
     /* Initialize  DateTime object. */
     $myDateTime = new \DateTime();
@@ -44,23 +46,31 @@ class OrderEventSubscriber implements EventSubscriberInterface {
     $subDate = $myDateTime->format('Y-m-d');
     /* Get current sub expiry date. */
     $field_subscription_expiry = $user->get('field_subscription_expiry')->getValue();
-    /* Is the Sub expiry date in the past? */
-    $oldSubExpiry = new \DateTime($field_subscription_expiry[0]['value']);
-    $days = $myDateTime->diff($oldSubExpiry)->format('%R%a');
-    if ($days > 0) {
-      $subExpiry = $oldSubExpiry->add(new \DateInterval('P1Y'))->format('Y-m-d');
+    /* Is the Sub expiry date in the past, in the future, or does it exist? */
+    if (isset($field_subscription_expiry[0]['value'])) {
+      $oldSubExpiry = new \DateTime($field_subscription_expiry[0]['value']);
+      $days = $myDateTime->diff($oldSubExpiry)->format('%R%a');
+      if ($days > 0) {
+        $subExpiry = $oldSubExpiry->add(new \DateInterval('P1Y'))->format('Y-m-d');
+      }
+      else {
+        $subExpiry = $myDateTime->add(new \DateInterval('P1Y'))->format('Y-m-d');
+      }
     }
     else {
       $subExpiry = $myDateTime->add(new \DateInterval('P1Y'))->format('Y-m-d');
     }
+    /* TODO: Prevent purchase of multiple subscriptions */
     foreach ($order->getItems() as $key => $order_item) {
       $product_variation = $order_item->getPurchasedEntity();
       $role = $product_variation->get('field_role')->getValue();
       $user->addRole($role[0]['value']);
       $user->set('field_subscription_date', $subDate);
       $user->set('field_subscription_expiry', $subExpiry);
-      $user->save();
     }
+    $user->save();
+    $message .= "Hello $username, you are logged in, and your subscription is now valid through $subExpiry";
+    \Drupal::messenger()->addMessage($message);
   }
 
 }
