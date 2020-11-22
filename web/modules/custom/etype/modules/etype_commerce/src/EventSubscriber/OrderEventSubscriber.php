64243f91151f12fb1dbc2bd079abc9c8d7eaa64d
Charlie Llewellin <charlie@llewellin.net>
2020-11-22 12:38:29 -0600

    Adding eType Commerce Module

--- web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
+++ web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
@@ -30,16 +30,29 @@ class OrderEventSubscriber implements EventSubscriberInterface {
    * @param \Drupal\state_machine\Event\WorkflowTransitionEvent $event
    *   The transition event.
    *
-   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Exception
    */
   public function onPlaceTransition(WorkflowTransitionEvent $event) {
     /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
     $order = $event->getEntity();
     $customer_id = $order->getCustomerId();
     $user = User::load($customer_id);
+    /* Extend subscription by 1 year from today if Sub Expiry is in past, or 1 year from Sub Expiry. */
+    /* Initialize  DateTime object. */
     $myDateTime = new \DateTime();
+    /* Set variable with new subscription date, today. */
     $subDate = $myDateTime->format('Y-m-d');
-    $subExpiry = $myDateTime->add(new \DateInterval('P1Y'))->format('Y-m-d');
+    /* Get current sub expiry date. */
+    $field_subscription_expiry = $user->get('field_subscription_expiry')->getValue();
+    /* Is the Sub expiry date in the past? */
+    $oldSubExpiry = new \DateTime($field_subscription_expiry[0]['value']);
+    $days = $myDateTime->diff($oldSubExpiry)->format('%R%a');
+    if ($days > 0) {
+      $subExpiry = $oldSubExpiry->add(new \DateInterval('P1Y'))->format('Y-m-d');
+    }
+    else {
+      $subExpiry = $myDateTime->add(new \DateInterval('P1Y'))->format('Y-m-d');
+    }
     foreach ($order->getItems() as $key => $order_item) {
       $product_variation = $order_item->getPurchasedEntity();
       $role = $product_variation->get('field_role')->getValue();
