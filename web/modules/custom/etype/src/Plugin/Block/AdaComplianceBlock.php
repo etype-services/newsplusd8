d619b1eca73a438008f6ee647aecc42f2ecfcbda
Charlie Llewellin <charlie@llewellin.net>
2020-12-07 09:31:37 -0600

    Ada Compliance Logo

--- web/modules/custom/etype/src/Plugin/Block/AdaComplianceBlock.php
+++ web/modules/custom/etype/src/Plugin/Block/AdaComplianceBlock.php
@@ -10,17 +10,16 @@ use Drupal\Core\Render\Markup;
  *
  * @Block(
  *   id = "ada_compliance_block",
- *   admin_label = @Translation("Date"),
+ *   admin_label = @Translation("ADA Compliance"),
  *   category = @Translation("eType"),
  * )
  */
 class AdaComplianceBlock extends BlockBase {
 
   /**
-   *
    * Uses Markup rather than '#markup' to return raw html.
    */
-  public function build() {
+  public function build(): array {
 
     $output = '
 <a href="/ada-compliance" aria-label="ADA Compliance Logo"><img src="/modules/custom/etype/img/AdaComplianceBlock.jpg" alt="This site complies with ADA requirements" /></a>
