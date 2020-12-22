c14a872114cc1c83e169be107a1ca1bfd00d37bb
Charlie Llewellin <charlie@llewellin.net>
2020-12-22 11:38:19 -0500

    Gift Subscription Form

--- web/modules/custom/etype/modules/etype_commerce/js/login-form.js
+++ web/modules/custom/etype/modules/etype_commerce/js/login-form.js
@@ -1,11 +1,18 @@
 (function ($, Drupal) {
   "use strict";
-  Drupal.behaviors.etypeCommerceLogin = {
+  Drupal.behaviors.etypeCommerce = {
     attach: function (context, settings) {
+      function handleSelectChange(event) {
+        let sel = event.target;
+        let val = sel.options[sel.selectedIndex].text;
+        alert(val);
+      }
       $("#edit-login-register-mail").addClass("input");
       $("#edit-login-returning-customer .fieldset-legend").html("Log in:");
       $("#edit-login-register .fieldset-legend").html("Create a new account:");
-      $("select").addClass("select");
+      $("select").addClass("select").once("etypeCommerce").change(function (event) {
+        handleSelectChange(event);
+      });
     }
   };
 })(jQuery, Drupal);
