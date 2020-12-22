660d44b94e51dbba587bfc6a4369a4b2a6e26dda
Charlie Llewellin <charlie@llewellin.net>
2020-12-22 07:22:29 -0500

    Gift Subscription Form

---
+++ web/modules/custom/etype/modules/etype_commerce/src/Form/GiftSubscriptionForm.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\etype_commerce\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\user\Entity\User;
+
+/**
+ * Class GiftSubscriptionForm provides access to free issue.
+ *
+ * @package Drupal\etype_commerce\Form
+ */
+class GiftSubscriptionForm extends FormBase {
+
+  /**
+   * Settings holder.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * GiftSubscriptionForm constructor.
+   */
+  public function __construct() {
+    $this->config = $config = \Drupal::config('etype_commerce.adminsettings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'gift_subscription_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter your email address'),
+      '#required' => TRUE,
+      '#default_value' => $form_state->getValue('email'),
+    ];
+
+    $form['username'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Choose a User Name'),
+      '#required' => TRUE,
+      '#default_value' => $form_state->getValue('username'),
+    ];
+
+    $form['password'] = [
+      '#type' => 'password',
+      '#title' => $this->t('Set a Password'),
+      '#required' => TRUE,
+      '#description' => "At least 8 characters",
+    ];
+
+    $form['confirmPassword'] = [
+      '#type' => 'password',
+      '#title' => $this->t('Confirm Your Password'),
+      '#required' => TRUE,
+    ];
+
+    $form['actions']['#type'] = 'actions';
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Register'),
+      '#button_type' => 'primary',
+    ];
+
+    return $form;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $username = $form_state->getValue('username');
+    $password = $form_state->getValue('password');
+    $confirmPassword = $form_state->getValue('confirmPassword');
+    $email = $form_state->getValue('email');
+
+    if (($user = user_load_by_name($username)) !== FALSE) {
+      $form_state->setErrorByName('username', $this->t('We can‘t create an account for you on this website because that user name already exists in our system. Please <a href="/user/login">login</a> or choose a different user name.'));
+    }
+    elseif (($user = user_load_by_name($email)) !== FALSE) {
+      $form_state->setErrorByName('email', $this->t('We can‘t create an account for you on this website because the email address supplied already exists in our system. Please <a href="/user/login">login</a> or use a different email address.'));
+    }
+    elseif (strlen($password) < 8) {
+      $form_state->setErrorByName('password', $this->t('Please select a password of at least 8 characters.'));
+    }
+    elseif (strcmp($password, $confirmPassword) !== 0) {
+      $form_state->setErrorByName('confirmPassword', $this->t('The passwords entered do not match. Please re-enter your preferred password.'));
+    }
+  }
+
+  /**
+   * Submit handler.
+   *
+   * @param array $form
+   *   The form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   Throw Exception.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /* Initialize  DateTime object. */
+    $myDateTime = new \DateTime();
+    $subExpiry = $myDateTime->add(new \DateInterval('P3D'))
+      ->format('Y-m-d');
+
+    // Register new user.
+    $email = $form_state->getValue('email');
+    $username = $form_state->getValue('username');
+    $password = $form_state->getValue('password');
+    $user = User::create();
+    $user->setPassword($password);
+    $user->enforceIsNew();
+    $user->setEmail($email);
+    $user->setUsername($username);
+    $user->set('field_subscription_expiry', $subExpiry);
+    $user->addRole('digital_subscriber');
+    $user->activate();
+    $user->save();
+    user_login_finalize($user);
+    \Drupal::messenger()->addMessage("Hello $username! Your account has been created and you are now logged in.");
+    $url = Url::fromRoute('<front>');
+    $form_state->setRedirectUrl($url);
+  }
+
+}
