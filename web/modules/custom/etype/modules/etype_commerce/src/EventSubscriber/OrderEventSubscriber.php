3c05884481afb37f58a38cba83f9e47aea22809a
Charlie Llewellin <charlie@llewellin.net>
2020-12-28 14:19:19 -0500

    Gift Subscription Processing

--- web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
+++ web/modules/custom/etype/modules/etype_commerce/src/EventSubscriber/OrderEventSubscriber.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\etype_commerce\EventSubscriber;
 
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\state_machine\Event\WorkflowTransitionEvent;
 use League\Csv\Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -18,6 +22,35 @@ use Drupal\commerce_cart\Event\CartEntityAddEvent;
  */
 class OrderEventSubscriber implements EventSubscriberInterface
 {
+
+  /**
+   * Success or failure message.
+   *
+   * @var string
+   */
+  protected string $message;
+
+  /**
+   * User role.
+   *
+   * @var string
+   */
+  protected string $role;
+
+  /**
+   * The Order.
+   *
+   * @var \Drupal\commerce_order\Entity\OrderInterface
+   */
+  protected OrderInterface $order;
+
+  /**
+   * OrderEventSubscriber constructor.
+   */
+  public function __construct() {
+    $this->message = '';
+    $this->role = '';
+  }
 
   /**
    * {@inheritdoc}
@@ -40,13 +73,51 @@ class OrderEventSubscriber implements EventSubscriberInterface
   public function onPlaceTransition(WorkflowTransitionEvent $event) {
     /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
     $order = $event->getEntity();
-    $customer_id = $order->getCustomerId();
-    $user = User::load($customer_id);
-    $username = $user->getUsername();
-    $message = '';
-    $subExpiry = '';
+    $this->order = $order;
+    $order_id = $this->order->id();
+    $customer_id = $this->order->getCustomerId();
 
-    /* Extend subscription by 1 year from today if Sub Expiry is in past, or 1 year from Sub Expiry. */
+    /* Is this a gift? */
+    $query = \Drupal::entityQuery('gift_subscription')
+      ->condition('order_id', $order_id);
+    $ids = $query->execute();
+    if (count($ids) > 0) {
+      /* This is a gift. There should only be one entity matching order id */
+      /* TODO: send email to gift subscriber */
+      $query = \Drupal::entityQuery('gift_subscription')
+        ->condition('order_id', $order_id);
+      $ids = $query->execute();
+      $entity = \Drupal::entityTypeManager()
+        ->getStorage('gift_subscription')
+        ->load(reset($ids));
+      $email = $entity->get('email')->getValue();
+      $gift_email = $email['value'];
+    }
+    else {
+      /*
+       * Not a gift.
+       * Extend subscription by 1 year from today if Sub Expiry is in past,
+       * or 1 year from Sub Expiry.
+       */
+      $this->extendSubscription($customer_id);
+    }
+
+  }
+
+  /**
+   * Extends User Subscription.
+   *
+   * @param int $uid
+   *   Customer Id.
+   *
+   * @throws \Exception
+   */
+  public function extendSubscription(int $uid) {
+    $user = User::load($uid);
+    $username = $user->getUsername();
+    $formatted_duration = '';
+    $formatted_role = '';
+
     /* Initialize  DateTime object. */
     $myDateTime = new \DateTime();
     /* Set variable with new subscription date, today. */
@@ -55,65 +126,76 @@ class OrderEventSubscriber implements EventSubscriberInterface
     $field_subscription_expiry = $user->get('field_subscription_expiry')
       ->getValue();
 
-    /* TODO: Prevent purchase of multiple subscriptions */
-
     /*
      * Loop over order items and process subscription.
      * There should only be one item
      */
-    foreach ($order->getItems() as $key => $order_item) {
+    foreach ($this->order->getItems() as $key => $order_item) {
       $product_variation = $order_item->getPurchasedEntity();
       $arr = $product_variation->get('attribute_subscription_type')->getValue();
       $target_id = ($arr[0]["target_id"]);
-      $entity = \Drupal::entityTypeManager()
-        ->getStorage('commerce_product_attribute')
-        ->load('subscription_type')
-        ->getValues();
-      $arr2 = $entity[$target_id]->name->getValue();
-      $role = $arr2[0]['value'];
+      try {
+        $entity = \Drupal::entityTypeManager()
+          ->getStorage('commerce_product_attribute')
+          ->load('subscription_type')
+          ->getValues();
+        $arr2 = $entity[$target_id]->name->getValue();
+        $this->role = $arr2[0]['value'];
+        switch ($this->role) {
+          case 'Print & Digital Subscriber';
+            $formatted_role = 'print_digital_subscriber';
+            break;
 
-      switch ($role) {
-        case 'Print & Digital Subscriber';
-          $formatted_role = 'print_digital_subscriber';
-          break;
-
-        default:
-          $formatted_role = 'digital_subscriber';
+          default:
+            $formatted_role = 'digital_subscriber';
+        }
+      }
+      catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
+        \Drupal::messenger()->addError('Could not load commerce_product_attribute entity');
       }
 
       $arr = $product_variation->get('attribute_duration')->getValue();
       $target_id = ($arr[0]["target_id"]);
-      $entity = \Drupal::entityTypeManager()
-        ->getStorage('commerce_product_attribute')
-        ->load('duration')
-        ->getValues();
-      $arr2 = $entity[$target_id]->name->getValue();
-      $duration = $arr2[0]['value'];
+      try {
+        $entity = \Drupal::entityTypeManager()
+          ->getStorage('commerce_product_attribute')
+          ->load('duration')
+          ->getValues();
+        $arr2 = $entity[$target_id]->name->getValue();
+        $duration = $arr2[0]['value'];
+        switch ($duration) {
+          case '1 year';
+            $formatted_duration = 'P1Y';
+            break;
 
-      switch ($duration) {
-        case '1 year';
-          $formatted_duration = 'P1Y';
-          break;
+          case '6 months';
+            $formatted_duration = 'P6M';
+            break;
 
-        case '6 months';
-          $formatted_duration = 'P6M';
-          break;
-
-        default:
-          $formatted_duration = 'P3D';
+          default:
+            $formatted_duration = 'P3D';
+        }
+      }
+      catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
+        \Drupal::messenger()->addError('Could not load commerce_product_attribute entity');
       }
 
       /* Is the Sub expiry date in the past, in the future, or does it exist? */
       if (isset($field_subscription_expiry[0]['value'])) {
-        $oldSubExpiry = new \DateTime($field_subscription_expiry[0]['value']);
-        $days = $myDateTime->diff($oldSubExpiry)->format('%R%a');
-        if ($days > 0) {
-          $subExpiry = $oldSubExpiry->add(new \DateInterval($formatted_duration))
-            ->format('Y-m-d');
+        try {
+          $oldSubExpiry = new \DateTime($field_subscription_expiry[0]['value']);
+          $days = $myDateTime->diff($oldSubExpiry)->format('%R%a');
+          if ($days > 0) {
+            $subExpiry = $oldSubExpiry->add(new \DateInterval($formatted_duration))
+              ->format('Y-m-d');
+          }
+          else {
+            $subExpiry = $myDateTime->add(new \DateInterval($formatted_duration))
+              ->format('Y-m-d');
+          }
         }
-        else {
-          $subExpiry = $myDateTime->add(new \DateInterval($formatted_duration))
-            ->format('Y-m-d');
+        catch (\Exception $e) {
+          \Drupal::messenger()->addError('Could not create DateTime object');
         }
       }
       else {
@@ -130,14 +212,13 @@ class OrderEventSubscriber implements EventSubscriberInterface
       if (!$user->save()) {
         throw new Exception("Unable to save user.");
       }
-      $message .= "Hello $username, you are logged in, and your subscription is now valid through $subExpiry";
-      \Drupal::messenger()->addMessage($message);
+      $this->message .= "Hello $username, you are logged in, and your subscription is now valid through $this->subExpiry";
+      \Drupal::messenger()->addMessage($this->message);
     }
-    catch (Exception $e) {
+    catch (Exception | EntityStorageException $e) {
       echo 'Caught Exception: ', $e->getMessage(), "\n";
       exit;
     }
-
   }
 
   /**
