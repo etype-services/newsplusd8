b730dca5e926586fe11545dcecc67333b5b0d6e0
Charlie Llewellin <charlie@llewellin.net>
2021-02-28 18:16:59 -0500

    Pico module, change redirect

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -1,12 +1,12 @@
 (function ($, Drupal) {
-    "use strict";
-    Drupal.behaviors.picoBehavior = {
-        attach: function (context, settings) {
-            $('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
-            $('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
-            $('.user-menu a[href="/pico-login"], .user-menu a[href="/pico-subscribe"]').on('click', function (event) {
-                event.preventDefault();
-            });
-        }
-    };
+  "use strict";
+  Drupal.behaviors.picoBehavior = {
+    attach: function (context, settings) {
+      //$('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
+      //$('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
+      $('.user-menu a[href="/pico-login"], .user-menu a[href="/pico-subscribe"]').on('click', function (event) {
+        event.preventDefault();
+      });
+    }
+  };
 })(jQuery, Drupal);
