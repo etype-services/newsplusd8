5631992a63ca39bcbfda62029bb741ced6626cc6
Charlie Llewellin <charlie@llewellin.net>
2021-02-28 18:40:04 -0500

    Pico module, change redirect

--- web/modules/custom/etype/modules/etype_pico/src/Plugin/Block/EtypePicoEeditionBlock.php
+++ web/modules/custom/etype/modules/etype_pico/src/Plugin/Block/EtypePicoEeditionBlock.php
@@ -7,6 +7,8 @@ use Drupal\Core\Block\BlockBase;
 
 /**
  * Provides a 'Etype Pico E-edition' Block.
+ * It's a duplicate of the eType Block.
+ * CSS Rules should make this visible only to non-paying Users.
  *
  * @Block(
  *   id = "etype_pico_block",
@@ -20,10 +22,10 @@ class EtypePicoEeditionBlock extends BlockBase {
     $e_editions = etype_e_editions();
     $output = '';
     foreach ($e_editions as $e_edition) {
-      $output .= '<div class="e-edition PicoSignal"><div><a class="PicoPlan" href="' . $e_edition['path'] . '" target="_blank" aria-label="' .
+      $output .= '<div class="e-edition PicoSignal"><div><a class="PicoPlan" href="#" target="_blank" aria-label="' .
         $e_edition['site_name'] . ' e-Edition"' . '><img src="' .
         $e_edition['image'] . '" alt="' . $e_edition['site_name'] . ' e-Edition"></a></div>
-<p><a class="PicoPlan" href="' . $e_edition['path'] . '" target="_blank" aria-label="' . $e_edition['site_name'] . ' e-Edition"' . '>Read ' . $e_edition['site_name'] . '</a></p></div>
+<p><a class="PicoPlan" href="#" target="_blank" aria-label="' . $e_edition['site_name'] . ' e-Edition"' . '>Read ' . $e_edition['site_name'] . '</a></p></div>
 ';
     }
     return [
