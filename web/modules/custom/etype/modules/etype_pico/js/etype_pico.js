d1ddf826e50b0af8c9007ab831861af7d79d3302
Charlie Llewellin <charlie@llewellin.net>
2021-03-01 11:49:36 -0500

    Pico module, change redirect

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico.js
@@ -2,8 +2,9 @@
   "use strict";
   Drupal.behaviors.picoBehavior = {
     attach: function (context, settings) {
-      //$('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
-      //$('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
+      $('.user-menu a[href="/pico-login"]').parent('li').addClass('PicoRule PicoSignal');
+      $('.user-menu a[href="/pico-subscribe"]').parent('li').addClass('PicoPlan PicoSignal');
+      $('.user-menu a[href="/e-edition"]').parent('li').addClass('PicoSignal');
       $('.user-menu a[href="/pico-login"], .user-menu a[href="/pico-subscribe"]').on('click', function (event) {
         event.preventDefault();
       });
