c25f0e7c22dfe805b71d7f5b77739485dba0d79f
Charlie Llewellin <charlie@llewellin.net>
2021-03-17 15:40:17 -0500

    Fix Pico

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -8,6 +8,7 @@
               let theAttr = theBlock.attr("data-pico-status");
               // console.log(theAttr);
               if (theAttr === 'paying') {
+                theBlock.html('<a href="' + drupalSettings.toknizdUrl + '">Load the e-Edition</a>');
                 window.location.replace(drupalSettings.toknizdUrl);
               } else {
                 if (drupalSettings.picoLandingPage.length > 1) {
