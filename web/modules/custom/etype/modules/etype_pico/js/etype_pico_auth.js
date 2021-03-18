e5f040aaf88daa1b02e886aa08b2dbdf2ef9bf47
Charlie Llewellin <charlie@llewellin.net>
2021-03-18 08:36:10 -0500

    Fix Pico

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -2,22 +2,11 @@
     "use strict";
     Drupal.behaviors.picoAuthBehavior = {
         attach: function (context, settings) {
-            // console.log(drupalSettings.toknizdUrl);
-            window.addEventListener('pico.loaded',function () {
-              let theBlock = $("#pico");
-              let theAttr = theBlock.attr("data-pico-status");
-              // console.log(theAttr);
-              if (theAttr === 'paying') {
-                theBlock.html('<a href="' + drupalSettings.toknizdUrl + '">Load the e-Edition</a>');
-                window.location.replace(drupalSettings.toknizdUrl);
-              } else {
-                if (drupalSettings.picoLandingPage.length > 1) {
-                  window.location.replace(drupalSettings.picoLandingPage);
-                } else {
-                  window.location.replace("/?pn=manage_payment");
-                }
-              }
-            });
+          if (drupalSettings.picoLandingPage.length > 1) {
+            window.location.replace(drupalSettings.picoLandingPage);
+          } else {
+            window.location.replace("/?pn=manage_payment");
+          }
         }
     };
 })(jQuery, Drupal, drupalSettings);
