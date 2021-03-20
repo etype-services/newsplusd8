4d6f4898a12be27feb19a8c9eb9ca1f2ab35c471
Charlie Llewellin <charlie@llewellin.net>
2021-03-20 11:33:14 -0500

    Move ADA button

--- web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
+++ web/modules/custom/etype/modules/etype_pico/js/etype_pico_auth.js
@@ -3,13 +3,12 @@
   Drupal.behaviors.picoAuthBehavior = {
     attach: function (context, settings) {
       // console.log(drupalSettings.toknizdUrl);
+      let theBlock = $("#pico");
       document.addEventListener('pico.loaded',function () {
-        let theBlock = $("#pico");
         let theAttr = theBlock.attr("data-pico-status");
         // console.log(theAttr);
         if (theAttr === 'paying') {
           // alert("Pico Loaded - paying customer");
-          // theBlock.html('<a href="' + drupalSettings.toknizdUrl + '">Go to the e-Edition</a>');
           location.replace(drupalSettings.toknizdUrl);
         } else {
           // alert("Pico Loaded - non-paying customer");
@@ -20,6 +19,10 @@
           }
         }
       });
+      let supportMessage = function () {
+        theBlock.html('<a href="' + drupalSettings.toknizdUrl + '">Click here to load the Digital Edition</a>');
+      };
+      setTimeout(supportMessage(), 5000);
     }
   };
 })(jQuery, Drupal, drupalSettings);
