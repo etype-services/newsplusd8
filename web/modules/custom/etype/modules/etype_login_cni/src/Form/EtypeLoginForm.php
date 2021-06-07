9fb2fcc80100a53060ed0c4f8278762e5a592c96
Charlie Llewellin <charlie@llewellin.net>
2021-06-07 12:55:44 -0500

    Etype CNI login error messages in EtypeLoginForm.php

--- web/modules/custom/etype/modules/etype_login_cni/src/Form/EtypeLoginForm.php
+++ web/modules/custom/etype/modules/etype_login_cni/src/Form/EtypeLoginForm.php
@@ -170,9 +170,11 @@ class EtypeLoginForm extends FormBase {
             // Clear cache to reset e-Edition links.
             Drupal::cache('menu')->invalidateAll();
             Drupal::service('plugin.manager.menu.link')->rebuild();
-            $redirectDestination = str_replace("http://", "http://", $base_url) . $_COOKIE["redirectDestination"];
-            $url = Url::fromUri($redirectDestination);
-            $form_state->setRedirectUrl($url);
+            if (isset($_COOKIE["redirectDestination"])) {
+              $redirectDestination = str_replace("http://", "http://", $base_url) . $_COOKIE["redirectDestination"];
+              $url = Url::fromUri($redirectDestination);
+              $form_state->setRedirectUrl($url);
+            }
         }
 
         break;
